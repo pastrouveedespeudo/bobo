4eb7f0b40761a1c1a2148474d6ed0f7b1e7e1baa
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-17 12:36:38 +0100

    Update views.py

--- photoo/views.py
+++ photoo/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from .photo import *
 
+
+def mes_images(request):
+    return render(request, 'mes_images.html')
+
+
+
+
 def photo(request):
     if request.method == "POST":
 
         recherche = request.POST.get('im')
-        print("OUIIIIIIIIIIIIII", recherche)
+        print(recherche,"000000000000000000000000000000000000000000000000000")
+        a = capture()
+        print(a,)
+        return render(request, 'mes_images.html', {'a':a})
 
-        capture()
+
+    
     return render(request, 'photo.html')
 
 
