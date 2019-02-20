a86de551a1b563cf8e4b0a8dbbaf580ef7893458
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-20 22:35:14 +0100

    Update vetement.py

--- photoo/vetement.py
+++ photoo/vetement.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 from PIL import Image
 
 
@@ -20,24 +21,29 @@ DICTIONNAIRE = {}
 
 #faudra ajuster les para ex: chignon ou frange
 
-def ajustage_couleur(dico):
+def ajustage_couleur(image):
 
     ok = "BLEU"
     pas_ok = "PAS BLUWW"
 
     liste = []
+    im = cv2.imread(str(image))
 
     for x in range(im.shape[0]):
         for y in range(im.shape[1]):
+
             if im[x,y][1] <= 80 and im[x,y][1] >= 0\
-               and im[x,y][2] >= 40 and im[x,y][2] <= 120:
-                
-                  
-             
-                return ok
-     
-            else:
-                return pas_ok
+               and im[x,y][2] >= 40 and im[x,y][2] <= 120\
+               and im[x,y][0] > [x,y][1] + 40 and im[x,y][0]> im[x,y][2] + 40:
+               
+
+
+                liste.append("ok")
+
+    if liste != [ ]:
+        return ok
+    else:
+        return pas_ok
         
     
     #ne marche que pour le bleu et les autre peut etre
@@ -69,16 +75,8 @@ class vetement:
 
         
         for i in liste:
-            dico = {}
-            im = Image.open(str(i))
-            for value in im.getdata(): 
-                if value in dico.keys():
-                    dico[value] += 1
-                else:
-                    dico[value] = 1
-               
 
-            ajustage = ajustage_couleur(dico)
+            ajustage = ajustage_couleur(i)
        
             if ajustage == "BLEU":
             
