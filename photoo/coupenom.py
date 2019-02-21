786ed45ad97a4ec8520ebf95f3b71ede2d68d982
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-21 15:51:32 +0100

    Update coupenom.py

--- photoo/coupenom.py
+++ photoo/coupenom.py
@@ -1,4 +1,6 @@
 import os
+import cv2
+
 
 def ajustage_couleur(image):#ici faudra faire genre si tu veux du vert l'ajuster
 
@@ -23,29 +25,6 @@ def ajustage_couleur(image):#ici faudra faire genre si tu veux du vert l'ajuster
         return pas_ok
 
 
-def ajustage_couleur_2(image, couleur, vertmin, vermax, rougemin, rougemax):
-
-    ok = couleur
-    pas_ok = "NOPE"
-
-    liste = []
-    im = cv2.imread(str(image))
-
-    for x in range(im.shape[0]):
-        for y in range(im.shape[1]):
-
-            if im[x,y][1] <= vertmin and im[x,y][1] >= vermax\
-               and im[x,y][2] >= rougemin and im[x,y][2] <= rougemax\
-               and im[x,y][0] > [x,y][1] + 40 and im[x,y][0]> im[x,y][2] + 40:
-               
-                liste.append("ok")
-    if liste != [ ]:
-        return ok
-    else:
-        return pas_ok
-
-
-
 def coupe_de_cheveux_nom(para):
 
     if para == "draggable14":
@@ -62,13 +41,14 @@ def coupe_de_cheveux_nom(para):
 
 def choix_fichier_haut(tenu):
     liste = []
-    valeur = list(para.values())
-    
+    valeur = list(tenu.values())
+    print(r"C:\Users\jeanbaptiste\bobo\bobo\static\img\portfolio\vetement\{}".format(valeur[0]))
     os.chdir(r"C:\Users\jeanbaptiste\bobo\bobo\static\img\portfolio\vetement\{}".format(valeur[0]))
     liste = os.listdir()
-    for i in liste: 
+    for i in liste:
+        print(i)
         ajustage = ajustage_couleur(i)
-        if ajustage == valeur_haut[1]:
+        if ajustage == valeur[1]:
             liste.append(str(i))
             
     return liste
