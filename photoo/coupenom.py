3fa3021144cb717f19a1a5ba0547bbe4a76b0975
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-21 19:44:59 +0100

    Update coupenom.py

--- photoo/coupenom.py
+++ photoo/coupenom.py
@@ -2,37 +2,16 @@ import os
 import cv2
 
 
-def ajustage_couleur(image):#ici faudra faire genre si tu veux du vert l'ajuster
-
-    ok = "BLEU"
-    pas_ok = "PAS BLUWW"
-
-    liste = []
-    im = cv2.imread(str(image))
-
-    for x in range(im.shape[0]):
-        for y in range(im.shape[1]):
-
-            if im[x,y][1] <= 80 and im[x,y][1] >= 0\
-               and im[x,y][2] >= 40 and im[x,y][2] <= 120\
-               and im[x,y][0] > [x,y][1] + 40 and im[x,y][0]> im[x,y][2] + 40:
-               
-                liste.append("ok")
-
-    if liste != [ ]:
-        return ok
-    else:
-        return pas_ok
 
 
 def coupe_de_cheveux_nom(para):
 
     if para == "draggable14":
 
-        print("c'est un chignon blond")
+    
         CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
-        DATE = ["2018 printemps"]
+ 
 
         #faire une base de donnée pour ca
 
@@ -41,20 +20,52 @@ def coupe_de_cheveux_nom(para):
 
 def choix_fichier_haut(tenu):
     liste = []
+    liste_vetement = []
     valeur = list(tenu.values())
-    print(r"C:\Users\jeanbaptiste\bobo\bobo\static\img\portfolio\vetement\{}".format(valeur[0]))
+    
     os.chdir(r"C:\Users\jeanbaptiste\bobo\bobo\static\img\portfolio\vetement\{}".format(valeur[0]))
     liste = os.listdir()
     for i in liste:
-        print(i)
-        ajustage = ajustage_couleur(i)
-        if ajustage == valeur[1]:
-            liste.append(str(i))
-            
-    return liste
+        ajustage = ajustage_couleur(i, valeur[1])
+        if ajustage == True:
+            print(i,"ouiouaiswokiou")
+            liste_vetement.append(i)
+
+    print(liste_vetement)
+    return ajustage
 
 
 
+
+def ajustage_couleur(image, couleur):
+
+    liste = []
+    validation = ""
+    im = cv2.imread(str(image))
+
+    if couleur == "bleu":
+
+        for x in range(im.shape[0]):
+            for y in range(im.shape[1]):
+
+                if im[x,y][1] <= 80 and im[x,y][1] >= 0\
+                   and im[x,y][2] >= 40 and im[x,y][2] <= 120\
+                   and im[x,y][0] > [x,y][1] + 40 and im[x,y][0]> im[x,y][2] + 40:
+                 
+                    validation = True
+                
+
+       
+        if validation == True:
+            print(image,"TRUREEEEEEEEEEEEEEEEE")
+            liste.append(image)
+        else:
+            validation == False
+
+
+        
+   
+    return validation
 
 
 
