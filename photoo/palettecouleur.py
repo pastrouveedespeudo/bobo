f7de3c00364e712b0545c51f68c7363918916bf9
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-22 19:08:12 +0100

    Add files via upload

---
+++ photoo/palettecouleur.py
@@ -0,0 +1,11 @@
+def couleur(couleur1, couleur2, couleur3):
+    if couleur1 >= 216 and couleur1<=255\
+       and couleur2 >= 105 and couleur2 <= 191\
+       and couleur3 <= 216 and couleur3 >= 150:
+        print("violet, rose")
+
+
+
+
+couleur(248, 159, 179)
+#http://www.discoveryplayground.com/computer-programming-for-kids/rgb-colors/
