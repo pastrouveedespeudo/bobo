1bbdb41641d1fc22237335bddac4815a4c9f9733
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-22 22:22:56 +0100

    Update palettecouleur.py

--- photoo/palettecouleur.py
+++ photoo/palettecouleur.py
@@ -2,32 +2,139 @@
 #faut refaire c chiant
 
 def couleur(couleur1, couleur2, couleur3):
-    if couleur1 >= 216 and couleur1<=255\
-       and couleur2 >= 105 and couleur2 <= 191\
-       and couleur3 <= 216 and couleur3 >= 150:
-        print("violet, rose")
 
-    if couleur1 >= 30 and couleur1 <= 255\
-       and couleur3 >= 30 and couleur3 <= 255\
-       and couleur2 <= couleur1 - 100 and couleur3 - 100:
+
+    if couleur1 >= 240 and couleur1 <= 250 \
+       and couleur2 >= 125 and couleur2 <= 218\
+       and couleur3 >= 125 and couleur3 <= 218 and\
+       couleur2 == couleur3:
+        print("rose")
+        
+    if couleur1 <= 255 and couleur1 >= 252 \
+       and couleur2 >= 145 and couleur2 <= 221\
+       and couleur3 >= 145 and couleur3 <= 211\
+       and couleur2 == couleur3:
         print("rose")
 
-    elif couleur1 <= 255 and >= 30\
-        and couleur2 <= 110 and couleur3 <= 70\
+        
+
+    if couleur1 <= 250 and couleur >= 238\
+       and couleur2 <= 219and couleur2 >= 138 \
+       and couleur3 <= 216 and couleur3 >= 128:
+
+        print('rose')
+
+
+    if couleur1 <= 195 and couleur >= 75\
+       and couleur2 <= 70 and couleur2 >= 24\
+       and couleur3 <= 70 and couleur3 >= 24\
+       and couleur1 >= couleur2 + 100 and couleur1 >= couleur3 + 100:
+        print("rouge")
+
+
+    if couleur1 <= and couleur >=\
+       and couleur2 <= and couleur2 >= \
+       and couleur3 <= and couleur3 >= :
+
+        
+    if couleur1 <=  177 and couleur >= 75\
+       and couleur2 <= 97 and couleur2 >= 37\
+       and couleur3 <= 92 and couleur3 >= 35\
+       and couleur3 >= couleur2 - 4:
+        print("rouge")
+
+
+    if couleur1 <= 180 and couleur >= 90\
+       and couleur2 <= 70 and couleur2 >= 50\
+       and couleur3 <= 87 and couleur3 >= 50\
+       and couleur2 == couleur3:
+        print("rouge")
+
+    if couleur1 >= 87 and couleur1 <= 213 \
+       and couleur2 >= 23 and couleur2 <= 80\
+       and couleur3 >= 23 and couleur3 <= 94\
+       and couleur2 == couleur3:
+        print("rouge")
+    
+
+    if couleur1 <= 237 and couleur1 >= 79\
+       and couleur2 >= 10 and couleur2 <= 90\
+       and couleur3 <= 90 and couleur3 >= 10\
+       and couleur1 >= couleur2 + 95 and couleur1 >= couleur3 + 95\
+       and couleur2 <= couleur3 - 20 :
+        print("rouge")
+
+    elif couleur1 <= 255 and >= 237\
+        and couleur2 <= 65 and couleur3 <=60:
+        print("rouge")
+
+    and couleur3 <= 70\
         and couleur1 >= couleur2 + 20 and couleur1 > couleur3 + 20:
         print("rouge")
+
         
+    if couleur1 <= 255 and couleur >= 241\
+       and couleur2 <= 244 and couleur2 >= 205\
+       and couleur3 <= 192 and couleur3 >= 138\
+       and couleur3 >= couleur2 -100:
+        print("jaune")
+
+    if couleur1 <= 255 and couleur >= 200\
+       and couleur2 <= 244 and couleur2 >= 200\
+       and couleur3 <= 140 and couleur3 >= 140:
+        print("jaune")
+
+
+    if couleur1 <= 255 and couleur >= 90\
+       and couleur2 <= 214 and couleur2 >= 77 \
+       and couleur3 == 1 or couleur3 == 2 :
+        print("jaune")
 
     elif couleur1 <= 255 and couleur1 >= 30\
          and couleur2 >= 60 and <= 240\
          and couleur3 <= 170:
         print("jaune")
-        
+
+    if couleur1 <= 105 and couleur >= 0\
+       and couleur2 <= 176 and couleur2 >= 80\
+       and couleur3 <= 110 and couleur3 >= 170\
+       and couleur2 >= couleur1 + 10:
+        print("vert")
+
+
     elif couleur1 <= 255 and couleur1 >= 30\
          and couleur2 >= 30 and couleur2 <= 255\
          and couleur3 >= 0 and couleur3 <= 180\
          and couleur2 >= couleur1 + 20 and couleur2 >= couleur1 + 20:
         print("vert")
+
+        
+    if couleur1 <= and couleur >=\
+       and couleur2 <=  and couleur2 >= \
+       and couleur3 <= and couleur3 >= :
+
+    if couleur1 <= and couleur >=\
+       and couleur2 <= and couleur2 >= \
+       and couleur3 <= and couleur3 >= :
+
+
+
+        
+    if couleur1 <= and couleur >=\
+       and couleur2 <= and couleur2 >= \
+       and couleur3 <= and couleur3 >= :
+
+    if couleur1 <= and couleur >=\
+       and couleur2 <= and couleur2 >= \
+       and couleur3 <= and couleur3 >= :
+
+    if couleur1 <= and couleur >=\
+       and couleur2 <= and couleur2 >= \
+       and couleur3 <= and couleur3 >= :
+
+
+        
+
 
     elif couleur1 <= 210 and couleur1 >= 0 \
          and couleur2 <= 200 and couleur2 >=0\
@@ -37,3 +144,4 @@ def couleur(couleur1, couleur2, couleur3):
          
 couleur(248, 159, 179)
 #http://www.discoveryplayground.com/computer-programming-for-kids/rgb-colors/
+#jve que ca sarrete j'en peux plus je sais meme pas si c juste nik je fais pas ligne par ligne
