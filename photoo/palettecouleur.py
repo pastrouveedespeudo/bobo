6c738dc9b10c7d7494da12b6afe6c8a8e2fa5f75
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-23 11:19:38 +0100

    Update palettecouleur.py

--- photoo/palettecouleur.py
+++ photoo/palettecouleur.py
@@ -66,17 +66,21 @@ def couleur(couleur1, couleur2, couleur3):
         and couleur3 <= 70\
         and couleur1 >= couleur2 + 20 and couleur1 > couleur3 + 20:
         print("rouge")
+        
+    elif couleur1 > couleur2 and couleur3:
+        print("rouge")
 
         
     if couleur1 <= 255 and couleur1 >= 241\
        and couleur2 <= 244 and couleur2 >= 205\
        and couleur3 <= 192 and couleur3 >= 138\
-       and couleur3 >= couleur2 -100:
+       and couleur3 >= couleur2 -100 and couleur2 >= couleur3:
         print("jaune")
 
     if couleur1 <= 255 and couleur1 >= 200\
        and couleur2 <= 244 and couleur2 >= 200\
-       and couleur3 <= 140 and couleur3 >= 140:
+       and couleur3 <= 140 and couleur3 >= 140\
+       and couleur1 > couleur2 and couleur2 > couleur3:
         print("jaune")
 
 
@@ -85,10 +89,12 @@ def couleur(couleur1, couleur2, couleur3):
        and couleur3 == 1 or couleur3 == 2 :
         print("jaune")
 
-    elif couleur1 <= 255 and couleur1 >= 30\
-         and couleur2 >= 60 and couleur2 <= 240\
-         and couleur3 <= 170:
-        print("jaune")
+    elif couleur1 >= 30 and couleur1  <= 255\
+         and couleur2 >= 100 and couleur2 <= 240\
+         and couleur3 <= 170 and\
+         couleur1 > couleur2 and couleur2 > couleur3 :
+        print("marron")
+
 
     if couleur1 <= 105 and couleur1 >= 0\
        and couleur2 <= 176 and couleur2 >= 80\
@@ -97,11 +103,24 @@ def couleur(couleur1, couleur2, couleur3):
         print("vert")
 
 
+    if couleur1 <= 190 and couleur1 >= 65\
+       and couleur2 <= 210 and couleur2 >= 80\
+       and couleur3 <= 165 and couleur3 >= 40\
+       and couleur2 > couleur1 and couleur1 > couleur3:
+        print("vert")
+
+    if couleur2 > couleur1 and\
+       couleur1 > couleur2:
+        print("vert")
+
+
+    if couleur3 > couleur2 and couleur2 > couleur1:
+        print("bleu")
+
     elif couleur1 <= 204 and couleur1 >= 150\
         and couleur2 <= 238 and couleur2 >= 220\
         and couleur3 <= 255 and couleur2 >= 220:
         print("bleu")
-
 
 
 
@@ -124,6 +143,10 @@ def couleur(couleur1, couleur2, couleur3):
          and couleur1 == couleur2 and couleur2>= couleur3 + 10 :
         print("bleu")
 
+    elif couleur1 < couleur2 and couleur1 < couleur3\
+         and couleur2 == couleur3:
+        print("bleu")
+
 
     elif couleur1 == 0\
         and couleur2 == 0\
@@ -142,13 +165,22 @@ def couleur(couleur1, couleur2, couleur3):
         print("vert")
 
 
+    elif couleur1 == couleur2 == couleur3 and couleur1 <= 42:
+        print("noir")
+         
+    elif couleur1 == couleur2 == couleur3 and couleur1 >= 220:
+        print("blanc")
 
-
+    elif couleur1 <= 177 and couleur1>= 74\
+         and couleur2 <= 147 and couleur2 >= 55\
+         and couleur3 <= 133 and couleur3 >= 47 and couleur3 >= couleur2 - 10\
+         and couleur2 >= couleur1 - 20:
+        print("marron")
 
 
 
          
-couleur(170, 180, 50)
+couleur(200, 100, 200)
 #http://www.discoveryplayground.com/computer-programming-for-kids/rgb-colors/
 
 
