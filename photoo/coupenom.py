5f9cac2141c226398d330515eb4d491b1d2c477b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-24 18:31:31 +0100

    Update coupenom.py

--- photoo/coupenom.py
+++ photoo/coupenom.py
@@ -1,43 +1,48 @@
 import os
 import cv2
+from colour import Color
 
 
-#la normalmeent tu fois faire une fonction qui apel la base de donnée
-#tu dois faire un truk qui rajoute aussi
-#bon au pire nik
+
+
+
+
+
+
+
 def coupe_de_cheveux_nom(para):
 
     if para == "draggable" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "rouge"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable1" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
-        CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "rouge"}
+        CHIGNON_BAS = {"bas":"jean", "couleurbas":"rouge"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable2" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "noir"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable3" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "blanc"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable4" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
-        CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "rouge"}
+        CHIGNON_BAS = {"bas":"jean", "couleurbas":"rouge"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
@@ -51,19 +56,19 @@ def coupe_de_cheveux_nom(para):
     
     elif para == "draggable6" :
         CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
-        CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
+        CHIGNON_BAS = {"bas":"jean", "couleurbas":"rouge"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable7" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
-        CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "orange"}
+        CHIGNON_BAS = {"bas":"jean", "couleurbas":"vert"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
     
     elif para == "draggable8" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "gris"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
@@ -78,25 +83,25 @@ def coupe_de_cheveux_nom(para):
     
     elif para == "draggable10" :
         CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
-        CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
+        CHIGNON_BAS = {"bas":"jean", "couleurbas":"marron"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable11" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "vert"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
     
     elif para == "draggable12" :
         CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
-        CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
+        CHIGNON_BAS = {"bas":"jean", "couleurbas":"blanc"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
     
     elif para == "draggable13" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "noir"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
@@ -109,21 +114,21 @@ def coupe_de_cheveux_nom(para):
         return CHIGNON_HAUT, CHIGNON_BAS
 
     elif para == "draggable15" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "vert"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable16" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "orange"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable17" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "gris"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
@@ -131,31 +136,20 @@ def coupe_de_cheveux_nom(para):
     
     elif para == "draggable18" :
         CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
-        CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
+        CHIGNON_BAS = {"bas":"jean", "couleurbas":"noir"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
     elif para == "draggable19" :
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
+        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "rouge"}
         CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
 
         return CHIGNON_HAUT, CHIGNON_BAS
 
     
 
-def coupe_de_cheveux_nom(para):
 
-    if para == "draggable14":
-
-    
-        CHIGNON_HAUT = {"haut":"t-shirt ", "hautcouleur": "bleu"}
-        CHIGNON_BAS = {"bas":"jean", "couleurbas":"bleu"}
- 
-
-        #faire une base de donnée pour ca
-
-        return CHIGNON_HAUT, CHIGNON_BAS
 
 
 def choix_fichier_haut(tenu):
@@ -193,21 +187,40 @@ def ajustage_couleur(image, couleur):
                  
                     validation = True
                 
+    if couleur == "rouge":
 
-       
+        for x in range(im.shape[0]):
+            for y in range(im.shape[1]):
+                
+                if im[x,y][0] <= 80 and im[x,y][0] >= 0\
+                   and im[x,y][1] >= 40 and im[x,y][1] <= 120\
+                   and im[x,y][2] > [x,y][1] + 40 and im[x,y][2]> im[x,y][0] + 40:
+
+                    validation = True
+                    break
+                
         if validation == True:
 
             liste.append(image)
         else:
             validation == False
 
-
-        
-   
+            
     return validation
 
 
 
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+print(rgb_to_hex((255, 255, 255))  )
+print(Color("#ff0000"))
+#fais tout crash
 
 
 
