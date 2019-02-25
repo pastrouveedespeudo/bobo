c40df98b0d781b757e3de9334d11068086368ffa
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-25 21:21:22 +0100

    Update a_ciel_batiment.py

--- polu/a_ciel_batiment.py
+++ polu/a_ciel_batiment.py
@@ -1,101 +1,150 @@
+import os
+import re
 import cv2
-from PIL import Image, ImageDraw, ImageChops
 import numpy as np
-from matplotlib import pyplot as plt
-
-class ciel_batiment_mask:
-
-
-    def ouverture_cv2(self, image):
-        self.image = image
-
-        img = cv2.imread(self.image)
-        return img
-
-    def ouverture_pil(self, image):
-        self.image = image
-
-        img = Image.open(self.image)
-        return img
-
-    def convert_gris(self, image):
-        self.image = image
-        img = Image.open(self.image).convert("LA")
-        img.save("convert.png")
-
-        return img
+from PIL import Image
+from statistics import mean
 
 
-    def otsu(self):
+class expé:
 
-        img = cv2.imread("polue1.jpg",0)
 
-        ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+
+    def roi(self):
         
-        plt.imshow(th2,'gray')
-        cv2.imwrite("otsu.png", th2)
-        plt.show()
+        im = cv2.imread("polue1.jpg")
+        #cv2.imshow("self.image1", im)
+        x = 140
+        y = 60
+        h = 40
+        w = 50
+        
+        x1 = 55
+        y1 = 10
+        h1 = 62
+        w1 = 55
 
-            
+        x2 = 1
+        y2 = 1
+        h2 = 50
+        w2 = 50
+        
+        cv2.rectangle(im, (x,y),(x+w,y+h),(0,0,255),2)
+        crop = im[y:y+h, x:x+w]
+        cv2.rectangle(im, (x1,y1),(x1+w1,y1+h1),(0,255,0),2)
+        crop2 = im[y1:y1+h1, x1:x1+w1]
+        cv2.rectangle(im, (x2,y2),(x2+w2,y2+h2),(255,0,0),2)
+        crop3 = im[y2:y2+h2, x2:x2+w2]
 
-    def prise_du_ciel_couleur(self, image):
-        self.image = image
 
+        cv2.imshow("self.image", im)
+        cv2.imshow("self.image2", crop)
+        
+        #cv2.imwrite("contour1.png", crop)
+        #cv2.imwrite("contour2.png", crop2)
+        #cv2.imwrite("contour3.png",crop3)
 
+    def image_gris(self):
+       
+        im = cv2.imread("1_1000_3000m3.jpg")
+        imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("yo.png", imgray)
+        cv2.imwrite("coucou.png", imgray)
 
-    def delimitation_haut_bas(self, image):
-        self.image = image
+        im = cv2.imread("coucou.png")
+     
 
+        x = 140
+        y = 60
+        h = 40
+        w = 50
+        
+        x1 = 55
+        y1 = 10
+        h1 = 62
+        w1 = 55
+
+        x2 = 1
+        y2 = 1
+        h2 = 50
+        w2 = 50
+        
+        cv2.rectangle(im, (x,y),(x+w,y+h),(0,0,255),2)
+        crop = im[y:y+h, x:x+w]
+        cv2.rectangle(im, (x1,y1),(x1+w1,y1+h1),(0,255,0),2)
+        crop2 = im[y1:y1+h1, x1:x1+w1]
+        cv2.rectangle(im, (x2,y2),(x2+w2,y2+h2),(255,0,0),2)
+        crop3 = im[y2:y2+h2, x2:x2+w2]
+
+        cv2.imshow("self.image", crop)
+        cv2.imshow("self.image2", crop2)
+        cv2.imshow("self.image3", crop3)
+        
+        cv2.imwrite("contour1_nb.png", crop)
+        cv2.imwrite("contour2_nb.png", crop2)
+        cv2.imwrite("contour3_nb.png",crop3)
 
         
-        
-        a = self.image[0 :self.image.shape[0]-1,10]
-        for x in range(self.image.shape[0]):
-            #print(self.image[x,300], x)
-            pass
-
-        #self.image[0 :self.image.shape[0]-1,300] = 255,255,255
+    def contraste(self):
+        #luminance0.2126*R+0.7152*V+0.0722*B
+        #faire la moyenne
+        #contraste = limage - lfond/lfond
 
         
-        self.image[329,300] = 255,255,255
+        liste_fond = []
+        liste_image_pres = []
+        liste_image_loin = []
         
+        #enelvee les roi
+        lumi_fond = cv2.imread("coucou.png")
+        for x in range(lumi_fond.shape[0]):
+            for y in range(lumi_fond.shape[1]):
+                a = lumi_fond[x,y][0] * 0.2126
+                b = lumi_fond[x,y][1] * 0.7152
+                c = lumi_fond[x,y][2] * 0.0722
+                total = a + b + c
+                liste_fond.append(total)
+                print(total)
+                
+        lumi_im_pres = cv2.imread("contour1_nb.png")
+        for x in range(lumi_im_pres.shape[0]):
+            for y in range(lumi_im_pres.shape[1]):
+                a = lumi_im_pres[x,y][0] * 0.2126
+                b = lumi_im_pres[x,y][1] * 0.7152
+                c = lumi_im_pres[x,y][2] * 0.0722
+                total = a + b + c
+                liste_image_pres.append(total)
+                print(total)
 
+        lumi_im_loin = cv2.imread("contour2_nb.png")
+        for x in range(lumi_im_loin.shape[0]):
+            for y in range(lumi_im_loin.shape[1]):
+                a = lumi_im_loin[x,y][0] * 0.2126
+                b = lumi_im_loin[x,y][1] * 0.7152
+                c = lumi_im_loin[x,y][2] * 0.0722
+                total = a + b + c
+                liste_image_loin.append(total)
+                print(total)
+
+
+        print(mean(liste_fond))
+        print(mean(liste_image_pres))
+        print(mean(liste_image_loin))
         
-        self.image[10,10] = 255,255,255
-        self.image[100,10] = 255,255,255
-
-        self.image[10,10] = 255,255,255
-        self.image[10,100] = 255,255,255
-
-        
-        cv2.imshow("self.image", self.image)
- 
-
-
-
-
-        
-
-
-
-
-
-if __name__ == "__main__":
-
-    yo = ciel_batiment_mask()
-    yo.ouverture_pil("polue2.jpg")
+        #bon au pire ca revient au meme
     
-    a = yo.ouverture_cv2("polue1.jpg")
-    b = yo.convert_gris("polue1.jpg")
-    yo.otsu()
-    
-    yo.delimitation_haut_bas(a)
-    
+        constrasteLocal = ""
 
 
+        #contrastelocal = limage - lcontour3/lcontour3
 
 
+        
+        
+yo = expé()
 
-
-
-
+yo.roi()
+#yo.image_gris()
+#yo.contraste()
+#bon ben enfete ca servait a rien de faire le truk otsu jcrois
