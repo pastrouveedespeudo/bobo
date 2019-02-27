fa37a1b605e51ae3b385b5dbd0f887028f2ab5c5
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-27 11:51:45 +0100

    Update pol.py

--- polu/pol.py
+++ polu/pol.py
@@ -1,6 +1,7 @@
 
 import os
 import cv2
+from colour import Color
 from PIL import Image, ImageDraw, ImageChops
 
 PATH_DOSSIER = r"C:\Users\jeanbaptiste\bobo\bobo\polution\26 frvrier 2019\apprentissage"
@@ -17,34 +18,91 @@ class couleur_ciel:
 
 
 
+    def hex_to_rgb(value):
+        value = value.lstrip('#')
+        lv = len(value)
+        return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+    def rgb_to_hex(rgb):
+        return '#%02x%02x%02x' % rgb
+        
+    def mask(self, image):
+        self.image = image
+        
+        img = Image.open(self.image)
+
+        
+        masque = Image.new('RGB', img.size, color=(255,255,255))
+
+        a = img.size[0] 
+        b = img.size[1] / 100 * 50
+
+        c = 0
+        d = 0
+
+        coords = (a,b, c,d)
+
+        masque_draw = ImageDraw.Draw(masque)
+        masque_draw.rectangle(coords, fill=(0,0,0))
+        diff = ImageChops.lighter(img, masque)
+
+        diff.save("ciel.jpg")
+        return "ciel.jpg"
 
     def ciel_terre(self, image):
         self.image = image
 
+        liste = []
         valeur_mask = []
 
-        img = cv2.imread(self.image)
-        imgrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(imgrey, 127, 255, cv2.THRESH_BINARY_INV)
-        
-        for x in range(thresh.shape[0]):
-            for y in range(thresh.shape[1]):
-                if thresh[x,y] == 255:
-                    valeur_mask.append(x,y)
-                    x = 0
-                    y += 1
-                    
-        #le but prendre une autre image soit la grise soit la couleur y appliquer
-                    #un contour bleu
-                    #faire le mask
-                    #prendre la couleur
-        cv2.imshow("image", img)
-
+        self.im = Image.open(self.image)
+                      
+        dico = {}
+        for value in self.im.getdata():
+            if value in dico.keys():
+                 dico[value] += 1
+            else:
+                 dico[value] = 1
     
-    #analyse couleur
-    #dire pourquoi cette couleur
 
+        liste_dico = []
+        for i in dico.values():
+            liste_dico.append(i)
+            
+        liste_dico= sorted(liste_dico, reverse = True)
 
+        for i in liste_dico:
+            liste.append([c for c,v in dico.items() if v==i])
+            if i <= 5:
+                break
+
+        
+        liste_dico = liste_dico[0:10]
+        liste_couleur = []
+        
+        for i in liste_dico:
+            
+            couleur = [c for c,v in dico.items() if v==i]
+            hex = couleur_ciel.rgb_to_hex((couleur[0]))
+            color = Color(hex)
+            liste_couleur.append(str(color))
+
+        return liste_couleur
+
+    def analyse_ciel_couleur(self, liste):
+        self.liste = liste
+        
+        print(self.liste)
+
+        for i in self.liste:
+            #if i[0] and i[1] and i[2] >= 0 and:
+                #CIEL['bleu'] += 1
+            #elif:
+            #else
+            pass
+            #ici faut définir les couleurs
+            
+        
 class meteo:
     METEO = {'beau_temps':0,
              'nuageux':0,
@@ -88,34 +146,10 @@ class fourtout:
           'inf90':0,
           'inf110':0,
           'inf130':0,
-            
-
         }
 
 
-        def mask(self, image):
-        self.image = image
-        
-        img = Image.open(self.image)
 
-        
-        masque = Image.new('RGB', img.size, color=(255,255,255))
-
-        a = img.size[0] 
-        b = img.size[1] / 100 * 70
-
-        c = 0
-        d = 0
-
-        coords = (a,b, c,d)
-   
-        
-        masque_draw = ImageDraw.Draw(masque)
-        masque_draw.rectangle(coords, fill=(0,0,0))
-        diff = ImageChops.lighter(img, masque)
-
-        diff.save("ciel.jpg")
-        return "ciel.jpg"
 
 
 
@@ -127,50 +161,14 @@ if __name__ == "__main__":
     print(liste_dossier)
     
     for i in liste_dossier:
-        if i == "pol.py" or i == "essais.py":
+        if i == "pol.py" or i == "essais.py" or i == "ciel.png":
             pass
         else:
             mask = yo.mask(i)
-            yo.ciel_terre(i)
-            
+            print(i)
+            couleur_du_ciel = yo.ciel_terre(mask)
+            yo.analyse_ciel_couleur(couleur_du_ciel)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
