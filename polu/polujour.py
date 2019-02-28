ba2f5f7b0bdda06d3acd69171ee0733f11034da9
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-28 22:59:32 +0100

    Update polujour.py

--- polu/polujour.py
+++ polu/polujour.py
@@ -14,13 +14,47 @@ mercredi 27 moyenne 15/18 degres PARIS il a fait bo
 19h 73-----14
 20h 72-----14
 21 72
-22
-84
+22 84
+
+
+28/02 13 
+23 105
+0 103
+1 97
+2 92
+3 89
+4 82
+5 85
+6 85
+7 33
+8 77
+9 59
+10 46
+11 48
+12 38
+13 36
+14 38
+15 40
+16 29
+17 33
+18 32
+19 36
+20 38
+21 31
+22 33 il a plu de 13a 14
+
+
+
+#aller encore 20 data comme ca et ca peut apsser y'a eu une baisse de pollution mais y mdis gros soleil,
+#cam tour eiffel == tout gris
+
+#ensuite y dmis si il a plus des torrents jcrois c lheure mais... ca fais 5h que jme dis ca doit et relheure et que jle crois pas ^^ 
 
 
 
 
 
+12h pluie jusu'a 14
 
 
 
@@ -42,10 +76,40 @@ MARSEIL il a fait bo
 16 50
 17 50
 18 49
-19 46
-20 47
-21 49
+19 62
+20 62
+21 94
 22 53
+
+
+
+28/02
+
+3 58
+4 58
+5 64
+6 63
+7 70
+8 78
+9 91
+10 94
+11 71
+12 64
+13 70
+14 103
+15 92
+16 73
+17 64
+18 57
+19 55
+20 30
+21 32
+22 34
+23
+
+
+
+
 
 LYON il a fait bo
 
@@ -67,6 +131,30 @@ LYON il a fait bo
 21 93
 22 93
 
+28/02
+
+23
+0 5
+1 5
+2 5
+3 5
+4 5
+5 5
+6 5
+7 5
+8 83
+9 81
+10 96
+11 105
+12 107
+13 50
+14 39
+15 44
+16 2
+17 28
+18 33
+19 41
+20
 
 
 
@@ -86,6 +174,67 @@ PEKIN heure local il a fait bo
 3 128
 4 128
 5 122
+
+28/02
+
+6 108
+7 110
+8 110
+9 104
+10 100
+11 91
+12 82
+13 104
+14 117
+15 124
+16 125
+17 167
+18 213
+19 249
+20 258
+21 257
+22 237
+23 237
+0 188
+1 189
+2 187
+3 168
+4 165
+5 163
+6
+7
+8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
