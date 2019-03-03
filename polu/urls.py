5edd7f283a549bf4f511a317a71efddf269dc8ff
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-03 11:36:14 +0100

    Update urls.py

--- polu/urls.py
+++ polu/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path(r'', views.home),
     path(r'/photo', include('photo.urls')),
     path(r'/polution', views.polution),
+    path(r'/charger', views.charger),
+    path(r'/polution_lyon', views.polution_lyon),
+    path(r'/polution_lyon', views.polution_marseille),
+    path(r'/polution_lyon', views.polution_paris),
 ]
