5211ba2aec7cecdcb7357d3425f59a836d732a13
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-03 14:50:35 +0100

    Update views.py

--- polu/views.py
+++ polu/views.py
@@ -21,22 +21,68 @@ def polution_lyon(request):
 
 
     a = heure()
+    temps = temps("lyon")
+    polution = particule("lieu")
+    
+
+    return render(request, 'polution_lyon.html', {'heure':a[0],'minute':a[1], 'temps':temps, 'pollution':polution})
+
+
+
+def polution_marseille(request):
+
+    a = heure()
+    temps = temps("marseille")
+    polution = particule("marseille")
+
+    
+    return render(request, 'polution_marseille.html', {'heure':a[0],'minute':a[1], 'temps':temps, 'pollution':polution})
+
+def polution_paris(request):
+
+    a = heure()
+    temps = temps("paris")
+    polution = particule("paris")
+    
+    return render(request, 'polution_paris.html', {'heure':a[0],'minute':a[1], 'temps':temps, 'pollution':polution})
+
+
+
+def heure():
+
+    date = datetime.datetime.now()
+
+    heure = date.hour
+    minute = date.minute
+
+    return heure, minute
+
+
+
+
+
+def temps(lieu):
 
     clé = '5a72ceae1feda40543d5844b2e04a205'
         
-    localisation = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}".format("lyon",clé)
+    localisation = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}".format(lieu,clé)
 
     r = requests.get(localisation)
 
     data=r.json()
-    print(data)
+    
     méteo = data['weather'][0]['main']
 
     if méteo == "Clear":
         méteo = "Beau"
 
+    return méteo
 
-    path = "https://air.plumelabs.com/fr/live/lyon"
+
+def particule(lieu)
+
+
+    path = "https://air.plumelabs.com/fr/live/{}".format(lieu)
 
     r = requests.get(path)
 
@@ -54,26 +100,25 @@ def polution_lyon(request):
     recherche_taux = str(liste).find(str(phrase_clé))
     liste_epluché = liste[20:21]
     polution = liste_epluché[0][31:33]
+
+    return polution
     
 
 
-    return render(request, 'polution_lyon.html', {'heure':a[0],'minute':a[1], 'temps':méteo, 'pollution':polution})
 
 
 
-def polution_marseille(request):
-    return render(request, 'polution_marseille.html')
-
-def polution_paris(request):
-    return render(request, 'polution_paris.html')
 
 
 
-def heure():
 
-    date = datetime.datetime.now()
 
-    heure = date.hour
-    minute = date.minute
 
-    return heure, minute
+
+
+
+
+
+
+
+
