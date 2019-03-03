296bc4bdb0e8fdb3fc9720aedf842a986668f0c2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-03 11:36:26 +0100

    Update views.py

--- polu/views.py
+++ polu/views.py
@@ -7,3 +7,15 @@ def home(request):
 
 def polution(request):
     return render(request, 'polution.html')
+
+def charger(request):
+    return render(request, 'charger.html')
+
+def polution_lyon(request):
+    return render(request, 'polution_lyon.html')
+
+def polution_marseille(request):
+    return render(request, 'polution_marseille.html')
+
+def polution_paris(request):
+    return render(request, 'polution_paris.html')
