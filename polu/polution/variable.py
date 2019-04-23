3f246d7a91e58ede94e93e562fbe2627221a596a
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-23 15:23:22 +0200

    Update variable.py

--- polu/polution/variable.py
+++ polu/polution/variable.py
@@ -13,3 +13,97 @@ VENT = {'tres fort':0,
         'moyen fort':0,
         'faible':0, 
 }
+
+REGION_INDUSTRIEL_POLLUEE = {'oui':0,
+                             'non':0,
+}
+
+PARTICULE = {'0_20':0,
+            '21_40':0,
+            '41_60':0,
+            '61_80':0,
+            '81_100':0,
+            '101_120':0,
+            '121_140':0,
+            '141_160':0,
+            '161_180':0,
+            '181_200':0,
+            '>200':0
+}
+
+
+VILLE_POLLUE2018 = {'VILLE_POLLUE2018':0,
+                    'un':0,
+                    'deux':0,
+                    'trois':0,
+                    'quattre':0,
+                    'non':0,
+}
+
+REGION_INDUSTRIEL_POLLUEE = {'oui':0,
+                             'non':0,
+}
+
+POPULATION_ACTIVE_HABITANT = {'sup1M':0,
+                              'sup500K':0,
+                              'supp300K':0,
+}
+
+
+TRAFIQUE = {'depart_routier':0,
+            'regulier jour':0,
+}
+
+HEURE = {'heure_pointe':0,
+        'non_heure_pointe':0,
+}
+
+POINTE = {'pointe':0,
+}
+
+WEEKEND = {'weekend':-2,
+}
+
+BOUCHON = {'non':0,
+           'petit':0,
+           'moyen':0,
+           'grand':0,
+           'assez grand':0,
+           'tres grand':0,
+}
+
+ACTIVITE_EXEPTIONNELLE = {'aggissement':0,
+                          'manifestation':0,
+                          'circulation dense':0,
+                          'condition a polution':0,
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
