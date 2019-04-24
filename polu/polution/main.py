5b90ef006dd7b751669953ade5fab5c03d4431ac
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-24 16:47:35 +0200

    Update main.py

--- polu/polution/main.py
+++ polu/polution/main.py
@@ -1,4 +1,5 @@
 import importlib
+import datetime
 
 from socio import socio
 from trafique import trafique
@@ -9,6 +10,8 @@ from climat import climat
 from variable import WEATHER
 from variable import WIND
 from variable import PRESSURE
+from variable import SAISON
+from variable import CLIMAT
 from variable import REGION_INDUSTRIEL_POLLUEE
 from variable import PARTICULE
 from variable import VILLE_POLLUE2018
@@ -25,34 +28,62 @@ from variable import LIST_CITY
 
 from traitement import display_dict
 from traitement import raise_dict
+from traitement import date_heure
+
+from config import HOST
+from config import USER
+from config import PASSWORD
+from config import DATABASE
+
+from database import insertion_table
+from database import visualisation_table
+
 
 class main:
 
+    
     def météologie(self):
+
+        referentiel = date_heure()
+
         
         for i in LIST_CITY:
             print(i)
 
-            météo.recuperation_donnée(self, i, WEATHER,WIND,PRESSURE)
+            météo.recuperation_donnée(self, i, WEATHER, WIND, PRESSURE)
             
-            display_dict(PRESSURE, WEATHER, WIND)
-            raise_dict(PRESSURE, WEATHER, WIND)
+            données = display_dict(PRESSURE, WEATHER, WIND)
 
+            insertion_table.insertion_meteo(self, i, referentiel[0], referentiel[1],
+                                            données[0], données[1], données[2])
+
+            raise_dict(PRESSURE, WEATHER, WIND)
             
+
+    def climat(self):
+        
+        for i in LIST_CITY:
+            print(i)
+
+            climat.recuperation_donnée(self, i, CLIMAT)
+            climat.saison(self, SAISON)
+            
+            display_dict(CLIMAT, SAISON)
+            raise_dict(CLIMAT, SAISON)   
+
 
     def pollution(self):
 
         for i in LIST_CITY:
             print(i)
             
-            particule.particule(self,i, PARTICULE)
             particule.france(self, i, VILLE_POLLUE2018)
             particule.industrie(self, i, REGION_INDUSTRIEL_POLLUEE)
 
-            display_dict(PARTICULE, VILLE_POLLUE2018,
+            display_dict(VILLE_POLLUE2018,
                          REGION_INDUSTRIEL_POLLUEE)
             
-            raise_dict(PARTICULE, VILLE_POLLUE2018,
+            raise_dict(VILLE_POLLUE2018,
                        REGION_INDUSTRIEL_POLLUEE)
 
 
@@ -72,29 +103,40 @@ class main:
         for i in LIST_CITY:
             print(i)
 
-            
             trafique.trafique_circulation(self, TRAFIQUE, HEURE)
             trafique.habitude(self, POINTE, WEEKEND)
             trafique.bouchons(self, i, BOUCHON)
             trafique.activité_execptionnelle(self, i, ACTIVITE_EXEPTIONNELLE)
             
-            display_dict(TRAFIQUE, HEURE, POINTE, WEEKEND, BOUCHON, ACTIVITE_EXEPTIONNELLE)
-            raise_dict(TRAFIQUE, HEURE, POINTE, WEEKEND, BOUCHON, ACTIVITE_EXEPTIONNELLE)
+            display_dict(TRAFIQUE, HEURE, POINTE, WEEKEND, BOUCHON,
+                         ACTIVITE_EXEPTIONNELLE)
+            
+            raise_dict(TRAFIQUE, HEURE, POINTE, WEEKEND, BOUCHON,
+                       ACTIVITE_EXEPTIONNELLE)
 
 
+    def particule(self):
 
+        print('\n')
+        for i in LIST_CITY:
+            print(i) 
 
-
-
+            particule.particule(self,i, PARTICULE)
+            
+            display_dict(PARTICULE)
+            raise_dict(PARTICULE)
 
 
 if __name__ == '__main__':
 
     main = main()
-    #main.météologie()
+    main.météologie()
+    #main.climat()
     #main.pollution()
     #main.sociologie()
-    main.trafic_routier()
+    #main.trafic_routier()
+
+    #main.particule()
 
 
 
