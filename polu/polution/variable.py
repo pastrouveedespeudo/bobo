d591752e2591f8c482e006582576c562945bad91
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-24 14:39:52 +0200

    Update variable.py

--- polu/polution/variable.py
+++ polu/polution/variable.py
@@ -1,105 +1,45 @@
+CLIMAT = {'> 0':0, '0_10':0, '11_20':0, '21_30':0, '31_40':0, '>40':0}
 
-CLIMAT = {'> 0':0,
-          '0_10':0,
-          '11_20':0,
-          '21_30':0,
-          '31_40':0,
-          '>40':0,
-}
+SAISON = {'primtemps':0, 'été':0, 'hiver':0, 'automne':0}
 
-SAISON = {'primtemps':0,
-          'été':0,
-          'hiver':0,
-          'automne':0,
-}
+WEATHER = {'beau_temps':0, 'nuageux':0, 'pluie':0}
 
+PRESSURE = {'forte':0, 'faible':0, 'normale':0}
 
-METEO = {'beau_temps':0,
-         'nuageux':0,
-         'pluie':0,
-}
+WIND = {'tres fort':0, 'fort':0, 'moyen fort':0, 'faible':0}
 
-PRESSION = {'forte':0,
-            'faible':0,
-            'normale':0,
-}
+REGION_INDUSTRIEL_POLLUEE = {'oui':0, 'non':0}
 
-VENT = {'tres fort':0,
-        'fort':0,
-        'moyen fort':0,
-        'faible':0, 
-}
-
-REGION_INDUSTRIEL_POLLUEE = {'oui':0,
-                             'non':0,
-}
-
-PARTICULE = {'0_20':0,
-            '21_40':0,
-            '41_60':0,
-            '61_80':0,
-            '81_100':0,
-            '101_120':0,
-            '121_140':0,
-            '141_160':0,
-            '161_180':0,
-            '181_200':0,
-            '>200':0
-}
+PARTICULE = {'0_20':0, '21_40':0, '41_60':0, '61_80':0, '81_100':0,
+            '101_120':0, '121_140':0, '141_160':0, '161_180':0,
+            '181_200':0, '>200':0}
 
 
-VILLE_POLLUE2018 = {'VILLE_POLLUE2018':0,
-                    'un':0,
-                    'deux':0,
-                    'trois':0,
-                    'quattre':0,
-                    'non':0,
-}
+VILLE_POLLUE2018 = {'VILLE_POLLUE2018':0, 'un':0, 'deux':0, 'trois':0,
+                    'quattre':0, 'non':0}
 
-REGION_INDUSTRIEL_POLLUEE = {'oui':0,
-                             'non':0,
-}
+REGION_INDUSTRIEL_POLLUEE = {'oui':0, 'non':0}
 
-POPULATION_ACTIVE_HABITANT = {'sup1M':0,
-                              'sup500K':0,
-                              'supp300K':0,
-}
+POPULATION_ACTIVE_HABITANT = {'sup1M':0, 'sup500K':0, 'supp300K':0}
 
 
-TRAFIQUE = {'depart_routier':0,
-            'regulier jour':0,
-}
+TRAFIQUE = {'depart_routier':0, 'regulier jour':0}
 
-HEURE = {'heure_pointe':0,
-        'non_heure_pointe':0,
-}
+HEURE = {'heure_pointe':0, 'non_heure_pointe':0}
 
-POINTE = {'pointe':0,
-}
+POINTE = {'pointe':0}
 
-WEEKEND = {'weekend':-2,
-}
+WEEKEND = {'weekend':0}
 
-BOUCHON = {'non':0,
-           'petit':0,
-           'moyen':0,
-           'grand':0,
-           'assez grand':0,
-           'tres grand':0,
-}
+BOUCHON = {'non':0, 'petit':0, 'moyen':0, 'grand':0, 'assez grand':0,
+           'tres grand':0}
 
-ACTIVITE_EXEPTIONNELLE = {'aggissement':0,
-                          'manifestation':0,
-                          'circulation dense':0,
-                          'condition a polution':0,
+ACTIVITE_EXEPTIONNELLE = {'aggissement':0, 'manifestation':0,
+                          'circulation dense':0, 'condition a polution':0}
 
 
 
-}
-
-
-
-
+LIST_CITY = ['lyon', 'paris', 'marseille']
 
 
 
