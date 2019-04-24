5c383fa83e92c636fdbf6c29a4f9d980e65cb3a2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-24 14:40:20 +0200

    Update main.py
    je sais pas quoi faire enfete du coup j'essaie des truk

--- polu/polution/main.py
+++ polu/polution/main.py
@@ -1,13 +1,14 @@
+import importlib
+
 from socio import socio
 from trafique import trafique
 from météo import météo
 from particule import particule
 from climat import climat
 
-
-from variable import METEO
-from variable import VENT
-from variable import PRESSION
+from variable import WEATHER
+from variable import WIND
+from variable import PRESSURE
 from variable import REGION_INDUSTRIEL_POLLUEE
 from variable import PARTICULE
 from variable import VILLE_POLLUE2018
@@ -20,40 +21,47 @@ from variable import WEEKEND
 from variable import BOUCHON
 from variable import ACTIVITE_EXEPTIONNELLE
 
+from variable import LIST_CITY
 
+from traitement import display_dict
+from traitement import raise_dict
 
 class main:
 
     def météologie(self):
-        météo.recuperation_donnée(self,
-                                  'paris',
-                                  METEO,
-                                  VENT,
-                                  PRESSION)
-        print(METEO)
-        print(VENT)
-        print(PRESSION)
-        
+
+        for i in LIST_CITY:
+            print(i)
+
+            météo.recuperation_donnée(self, i, WEATHER,WIND,PRESSURE)
+            
+            displaying = display_dict(PRESSURE, WEATHER, WIND)
+            
+            raise_dict(PRESSURE, WEATHER, WIND)
+
+            print('\n')
 
     def pollution(self):
-        particule.particule(self,'paris', PARTICULE)
         
+        particule.particule(self,'paris', PARTICULE)
         particule.france(self, 'paris', VILLE_POLLUE2018)
         particule.industrie(self, 'paris', REGION_INDUSTRIEL_POLLUEE)
 
-        
         print(PARTICULE)
         print(VILLE_POLLUE2018)
         print(REGION_INDUSTRIEL_POLLUEE)
 
 
     def sociologie(self):
+        
         socio.habitant(self, 'paris', POPULATION_ACTIVE_HABITANT)
+        
         print(POPULATION_ACTIVE_HABITANT)
 
 
 
     def trafic_routier(self):
+        
         trafique.trafique_circulation(self, TRAFIQUE, HEURE)
         trafique.habitude(self, POINTE, WEEKEND)
         trafique.bouchons(self, 'paris', BOUCHON)
@@ -75,10 +83,10 @@ class main:
 if __name__ == '__main__':
 
     main = main()
-    #main.météologie()
+    main.météologie()
     #main.pollution()
     #main.sociologie()
-    main.trafic_routier()
+    #main.trafic_routier()
 
 
 
