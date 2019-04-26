b15deaceb32e4bda712050e11fea384f3db6c551
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-26 10:39:17 +0200

    Update traitement_de_donnée.py
    je suis perduuuuuuu

--- polu/polution/traitement_de_donnée.py
+++ polu/polution/traitement_de_donnée.py
@@ -1,11 +1,14 @@
 from database import visualisation_table
+from database import creation_conditions
 
 from variable import LIST_CITY
 
-class data:
-    def recup_météo_by_particle(self):
+liste = ['paris']
 
-        for i in LIST_CITY:
+class data:
+    def recup_data_by_particle(self):
+
+        for i in liste:
             print('\n')
             print(i)
             
@@ -13,8 +16,74 @@ class data:
             for i in meteo:
                 print(i)
 
+    def visualisation_without_time(self):
+        for ville in liste:
+            print('\n')
+            print(ville)
+            data = creation_conditions.visualisation_without_time(self, ville)
+            data = set(data)
+            c = 0
+            for i in data:
+                print(i)
+
+                id_data = creation_conditions.recuperate_id(self, ville,
+                                                            i[0],i[1],i[2],i[3],
+                                                            i[4],i[5],i[6],i[7],
+                                                            i[8],i[9],i[10],i[11],
+                                                            i[12],i[13])
+
+                print(id_data)
+                c+=1
+            
+            print(c)
+            
 
 
 
+
+
+
+    def recup_id(self):
+        pass
+
+
+
+            
 data = data()
-data.recup_météo_by_particle()
+#data.recup_data_by_particle()
+data.visualisation_without_time()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
