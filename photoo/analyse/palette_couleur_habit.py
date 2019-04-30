2bec952ec9ed7bdaa9c5c5fae555d63a7657abee
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-30 15:24:09 +0200

    Add files via upload

---
+++ photoo/analyse/palette_couleur_habit.py
@@ -0,0 +1,393 @@
+
+
+def palette(couleur):
+
+    if couleur == '#d7d3d0' or couleur == '#d1d1d1' or couleur == '#d3d4cf' or\
+       couleur == '#cfcecc' or couleur == '#d6d1ce' or couleur == '#d3d0cb' or\
+       couleur == '#d4d1cc' or couleur == '#d3cfcc' or couleur == '#d7d3d0' or\
+       couleur == '#d5d1d2' or couleur == '#d4d0d1' or couleur == '#d2ccce' or\
+       couleur == '#d3cdcf' or couleur == '#d5cfd1' or couleur == '#d3cfd0' or\
+       couleur == '#d4ced0' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e0e1e6' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e1e0e8' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#55565a' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or:
+        print('gris')
+        return 'gris blanc'
+
+    if couleur == '#e2dfea' or couleur == '#e7e4ef' or couleur == '#e6e3ee' or\
+       couleur == '#e1d9e6' or couleur == '#eff3f6' or couleur == '#ddd5e2' or\
+       couleur == '#ded6e3' or couleur == '#dfd7e4' or couleur == '#e2dce8' or\
+       couleur == '#e0dae6' or couleur == '#e3dde9' or couleur == '#e4deea' or couleur == '' or\
+       couleur == '#e3e0eb' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e4e1ec' or couleur == '#e5e2ed' or couleur == '#e1dee9' or couleur == '' or\
+       couleur == '#dcd6e2' or couleur == '' or couleur == '#dddae5' or couleur == '#9fa0a4' or\
+       couleur == '#e4e3e9' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e6e4ef' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e6e7eb' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e7e8ed' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '#ebedf9' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#edeef2' or couleur == '' or couleur == '' or couleur == '' or\
+        print('bleu')
+        return ''
+    if couleur == '#d4d0cd' or couleur == '#bbbcc0' or couleur == '#d3cdcf' or\
+       couleur == '#76777b' or couleur == '#505155' or couleur == '' or couleur == '' or\
+       couleur == '#dddce2' or couleur == '#e5e2ed' or couleur == '' or couleur == '' or\
+       couleur == '#919296' or couleur == '#b8b9bd' or couleur == '' or couleur == '' or\
+       couleur == '#d1d2d7' or couleur == '#4b4c50' or couleur == '' or couleur == '' or\
+       couleur == '#4f5054' or couleur == '#4c4d51' or couleur == '' or couleur == '' or\
+       couleur == '#535458' or couleur == '#c9cace' or couleur == '' or couleur == '' or\
+       couleur == '#949599' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#88898d' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#cccdd2' or couleur == '' or couleur == '' or couleur == '':
+        print('gris')
+        return ''
+
+    if couleur == '#d9cedc' or couleur == '#d6cbd9' or couleur == '#e0d8e5' or\
+       couleur == '#d4c9d7' couleur == '#d7ccda' or couleur == '#dcd1df' or couleur == ''or\
+       couleur == '#d5cdda' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#ddd7e3' or couleur == '#d8d0dd' or couleur == '#dfd9e5' or couleur == '#dedbe6' or\
+       couleur == '#ded8e4' or couleur == '#dcd4e1' or couleur == '' or couleur == '' or\
+       couleur == '#ded6e3' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e2e0eb' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#ddd8df' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#dcd4e1' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#dbd1da' or couleur == '' or couleur == '' or couleur == '':
+        print('violet')
+        return ''
+    
+    if couleur == '#e0e3f2' or couleur == '#e0dde8' or couleur == '#e4e8f1' or couleur == '#e9f1f4' or\
+       couleur == '#eef2fb' or couleur == '#eff3f6' or couleur == '' or couleur == '' or\
+       couleur == '#e4e7f6' or couleur == '#dedce7' or couleur == '' or couleur == '' or\
+       couleur == '#eeeff3' or couleur == '#ded8e4' or couleur == '' or couleur == '' or\
+       couleur == '#e8e8f2' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e5e5f1' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e5e5ef' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#e8e8f0' or couleur == '' or couleur == '' or couleur == '' or\:
+        print('bleu')
+        return ''
+
+    if couleur == '#dbd3e0'couleur == '#d9cedc' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\:
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\:
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\:
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\:
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\:
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\:
+    
+        print('bleu teinte violet')
+        return ''
+    if couleur == '#434448':
+        print('noir')
+        return ''
+
+    if couleur == '#d3d5e1' or couleur == '#dee0ec' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#d4d3e3' or couleur == '#e9ebf7' or couleur == '' or couleur == '' or\
+       couleur == '#dfe1ee' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#ecebf1' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#eef1f8' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '':
+        print('bleu')
+        return ''
+    
+    if couleur == '#ededf5' or couleur == '#ebf0f6' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#eeeff3' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#eff2f9' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '#edecf2' or couleur == '' or couleur == '' or couleur == '':
+               
+        print('blanc')
+        return ''
+
+    if couleur == '#ebeaef' or\
+       couleur == '#e3e6ef' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '':
+        print('blanc')
+        return ''
+    
+    if couleur == '#d6d1ce' or\
+       couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '' or\
+       couleur == '' or couleur == '' or couleur == '' or couleur == '':
+        print('beige')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    if couleur == '':
+        print('')
+        return ''
+
+    if couleur == '':
+        print('')
+        return ''
+    
+
+
+
+##    
+##    if couleur1 > 240 and\
+##       couleur2 > 240 and\
+##       couleur3 > 240 and\
