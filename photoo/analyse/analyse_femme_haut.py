29b0a724cdf807df59d42181e03597a7a36182b7
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-01 12:08:56 +0200

    Update analyse_femme_haut.py

--- photoo/analyse/analyse_femme_haut.py
+++ photoo/analyse/analyse_femme_haut.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import cv2
+from colour import Color
 import numpy as np
 from PIL import Image, ImageDraw, ImageChops
 from matplotlib import pyplot as plt
@@ -8,8 +9,13 @@ import math
 
 from constante import HAAR_FACE
 from constante import HAAR_YEUX
+from constante import IMAGE_TRAITEMENT_HAUT
+from constante import IMAGE_TRAITEMENT_BAS
 
-from palettecouleur import *
+from palettecouleur_coiffure import *
+from coul import *
+from palettecouleur import DICO_COULEUR
+
 
 ##        for x in range(image.shape[0]):
 ##            for y in range(image.shape[1]):
@@ -46,9 +52,6 @@ class image_femme_haut:
 
         coords = (a,b, c,d)
    
-
-
-        
         masque_draw = ImageDraw.Draw(masque)
         masque_draw.rectangle(coords, fill=(0,0,0))
         diff = ImageChops.lighter(img, masque)
@@ -62,9 +65,9 @@ class image_femme_haut:
 
         masque = Image.new('RGB', img.size, color=(255,255,255))
 
-        a = img.size[1] 
-        b = img.size[0] / 100 * 130
-
+        a = img.size[1]
+        b = img.size[0] / 100 * 100
+       
         c = 0
         d = 0
 
@@ -74,9 +77,54 @@ class image_femme_haut:
         masque_draw = ImageDraw.Draw(masque)
         masque_draw.rectangle(coords, fill=(0,0,0))
         diff = ImageChops.lighter(img, masque)
+  
+ 
+        img.crop((0, 0, b, a/2)).save('traitement_haut.jpg')
 
-        diff.save("traitement_haut.jpg")
-       
+
+    def logo(self, image):
+        pass
+
+
+    def couleur_habit(self, image):
+        self.image = image
+
+        dico = {'bleu':0,'beige':0,'marron beige':0,'marron':0,'gris':0,
+                'marron gris':0,'vert':0,'bleu clair':0,'jaune':0,'orange':0,
+                'noir':0,'bleu violet':0,'beige foncé':0,'blanc':0,'kaki':0,
+                'vert foncé':0,'orange foncé':0,'jaune foncé':0,'turquoise':0,
+                'bleu':0,'rose':0,'rouge':0,'magenta':0}
+
+        image = cv2.imread(self.image)
+        for x in range(image.shape[0]):
+            for y in range(image.shape[1]):
+                
+                colordb = get_colordb('bobo.txt')
+                if not colordb:
+                    print('No parseable color database found')
+                    sys.exit(1)
+                nearest = colordb.nearest(image[x,y][2],
+                                          image[x,y][1],
+                                          image[x,y][0])
+    
+                for clé, valeur in DICO_COULEUR.items():
+                    if nearest == clé:
+                        for clé1, valeur1 in dico.items():
+                            if clé1 == DICO_COULEUR[nearest]:
+                                dico[clé1] += 1
+        print(dico)
+
+
+        #print(dico)
+        #cv2.imshow('image', image)
+
+
+
+
+
+
+
+
     def couleur_cheveux(self, image):
         self.image = image
 
@@ -92,7 +140,7 @@ class image_femme_haut:
              else:
                  dico[value] = 1
 
-
+        print(dico)
         liste = []
         
         for cle, valeur in dico.items():
@@ -151,14 +199,13 @@ class image_femme_haut:
             return 'chatin'
 
 
-    def couleur_habit(self):
-        pass
+
 
 
             
     def essais(self):
-        im = cv2.imread('5b.jpg')
-        print(im[50:90,50])
+        im = cv2.imread('traitement_haut.jpg')
+        im[50:90,50] = 254,107,0
         cv2.imshow('image', im)
     
 if __name__ == '__main__':
@@ -166,13 +213,20 @@ if __name__ == '__main__':
     
     image_femme_haut = image_femme_haut()
 
-    #image_femme_haut.resize('9.jpg', '9.jpg')
-
-    IMAGE = '5a.jpg'
     
-    image_femme_haut.mask_bas(IMAGE)
-    image_femme_haut.mask_haut(IMAGE)
-    image_femme_haut.couleur_cheveux('6b.jpg')
+
+    IMAGE = '2a.jpg'
+    
+    #image_femme_haut.mask_bas(IMAGE)
+    #image_femme_haut.mask_haut(IMAGE)
+    
+    image_femme_haut.resize(IMAGE_TRAITEMENT_HAUT, 'traitement_haut1.jpg')
+    
+    image_femme_haut.couleur_habit('traitement_haut1.jpg')
+    
+
+    
+    #image_femme_haut.couleur_cheveux('6b.jpg')
 
     #image_femme_haut.essais()
 
