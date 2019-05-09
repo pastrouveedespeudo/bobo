272210e4f08aa4c0b875af7aa1c401345219afa0
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-09 22:37:38 +0200

    Update views.py
    passer au template d'un coté les coiffures de l'autre les favoris, gauche favoris = analyse droite les coiffures

--- photoo/views.py
+++ photoo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+
 
 from .photo import *
 from .coupenom import *
@@ -15,27 +17,31 @@ def mes_images(request):
 
     liste1 = []
     liste2 = []
+    
     image = Accounts.objects.filter(name=current_user).all()
     for i in image:
         print(i.photo_habit, i.photo_cheveux)
         liste1.append(i.photo_habit)
         liste2.append(i.photo_cheveux)
-    print(liste2)
-    print('\n')
+        
+
     print(liste1)
+    
     liste11 = []
     liste22 = []
+    
     for i in liste1:
-        print(i)
         if i == '':
             pass
         else:
             liste11.append(i)
+            
     for i in liste2:
         if i == '':
             pass
         else:
             liste22.append(i)
+            
     return render(request, 'mes_images.html', {'user':current_user, 'liste1':liste11,
                                                'liste2':liste22})
 
@@ -88,12 +94,14 @@ def photo(request):
 
         liste11 = []
         liste22 = []
+        
         for i in liste1:
             print(i)
             if i == '':
                 pass
             else:
                 liste11.append(i)
+                
         for i in liste2:
             if i == '':
                 pass
@@ -111,18 +119,46 @@ def photo(request):
 def essais(request):
     return render(request, 'essais.html')
 
+
+
+@csrf_protect
 def coupe(request):
 
     no_choice = 'no_choice'
 
-
+    current_user = request.user
+    
+    affichage_coupe_fav(current_user)
+    
     if request.method == "POST":
-        
+
+        print('OUIIIIIIIIIIIII')
         image = request.POST.get('posting')
         coupe = request.POST.get('coupe')
         recherche = request.POST.get('coupedecheveux')
+        
+        enregistement = request.POST.get('produit')
+ 
         print(recherche,'7777777777777777777777777778888')
         print(coupe,'777777777777777777777777777777')
+        print(enregistement,'8888888888888897978979879878979')
+
+        
+
+        liste_enre = [[],[],[]]
+        c = 0
+        if enregistement:
+            for i in enregistement:
+                if i == ',':
+                    c+=1
+                else:
+                    liste_enre[c].append(i)
+
+            
+            coupe_fav(current_user, "".join(liste_enre[0]),
+                      "".join(liste_enre[1]),
+                      "".join(liste_enre[2]))
+            
 
         if recherche:
             for cle, value in DICO_COIF.items():
@@ -134,11 +170,17 @@ def coupe(request):
             print(image,"0100000000000000000000000000000000000")
             current_user = request.user
         
-            return render(request, 'coupe.html', {'image':image, 'user':current_user})
+            return render(request, 'coupe.html', {'image':image, 'user':current_user,
+                                                  affichage_coupe_fav[0],
+                                                  affichage_coupe_fav[1],
+                                                  affichage_coupe_fav[2]}})
         
 
 
-    return render(request, 'coupe.html', {'no_choice':no_choice})
+    return render(request, 'coupe.html', {'no_choice':no_choice,
+                                          affichage_coupe_fav[0],
+                                          affichage_coupe_fav[1],
+                                          affichage_coupe_fav[2]})
 
 def habits(request):
     return render(request, 'habits.html')
