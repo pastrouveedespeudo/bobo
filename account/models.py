79a0b2e27a37bfa79de444e60c9869dfc54fb85e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-09 15:25:35 +0200

    Update models.py

--- account/models.py
+++ account/models.py
@@ -4,6 +4,9 @@ class Accounts(models.Model):
     """foodAccount model"""
     
     name = models.CharField(max_length=50)
-    photo = models.ImageField()
+    path_image = models.CharField(max_length=100)
+    photo_habit = models.ImageField()
+    photo_cheveux = models.ImageField()
     habits = models.CharField(max_length=100)
     coiffure = models.CharField(max_length=100)
+
