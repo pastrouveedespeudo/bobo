ef15f17530d00329887e45a4b7510e93723863cc
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-12 01:51:27 +0200

    Update views.py

--- administrateur/views.py
+++ administrateur/views.py
@@ -1,28 +1,22 @@
 from django.shortcuts import render
 import os
 
+
+from .analysis_psychopg2.mode_analyse import *
+
+
+
 def mode(request):
     return render(request, "mode.html")
 
 
 def database_mode(request):
 
-    os.chdir(r'C:\Users\jeanbaptiste\bobo\bobo\administrateur\analysis_psychopg2')
-    liste = os.listdir()
+    data = recup()
+    print(data)
+    print('ouiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
 
-    for i in liste:
-        if i == '__pycache__' or i == 'analyse_femme_haut.py' or\
-           i == 'constante.py' or i == 'coul.py' or\
-           i == 'palettecouleur.py' or i == 'palettecouleur_coiffure.py' or\
-           i=='bobo.txt' or i== 'traitement_haut.jpg' or i == "traitement_bas1.jpg" or\
-           i == 'config.py' or i == 'constante.py' or i =='coul.py' or i == 'database.py' or\
-           i == 'palettecouleur.py' or i =='palettecouleur_coiffure.py':
-            pass
-
-        else:
-            print(i)
-    
-    return render(request, "database_mode.html")
+    return render(request, "database_mode.html", {'data':data})
 
 
 def tendance(request):
