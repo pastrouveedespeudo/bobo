f6af3a56564b92d349304c6f175d81f589d6398e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-11 21:12:39 +0200

    Update views.py
    ok le but mtn c de tout afficher que ce soit les images: vetement coupe associé
    puis le haut et le bas (donc refaire un crop) puis leur couleur

--- administrateur/views.py
+++ administrateur/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+import os
 
 def mode(request):
     return render(request, "mode.html")
 
 
 def database_mode(request):
+
+    os.chdir(r'C:\Users\jeanbaptiste\bobo\bobo\administrateur\analysis_psychopg2')
+    liste = os.listdir()
+
+    for i in liste:
+        if i == '__pycache__' or i == 'analyse_femme_haut.py' or\
+           i == 'constante.py' or i == 'coul.py' or\
+           i == 'palettecouleur.py' or i == 'palettecouleur_coiffure.py' or\
+           i=='bobo.txt' or i== 'traitement_haut.jpg' or i == "traitement_bas1.jpg" or\
+           i == 'config.py' or i == 'constante.py' or i =='coul.py' or i == 'database.py' or\
+           i == 'palettecouleur.py' or i =='palettecouleur_coiffure.py':
+            pass
+
+        else:
+            print(i)
+    
     return render(request, "database_mode.html")
 
 
