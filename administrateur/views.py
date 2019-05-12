d0f038f814de73b03fe436c8aa54e76b56a70e12
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-12 16:49:14 +0200

    Update views.py

--- administrateur/views.py
+++ administrateur/views.py
@@ -5,6 +5,8 @@ import os
 from .analysis_psychopg2.mode_analyse import *
 
 
+from .analysis_psychopg2.mode_w_data import haut_bas
+
 
 def mode(request):
     return render(request, "mode.html")
@@ -16,6 +18,11 @@ def database_mode(request):
     print(data)
     print('ouiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
 
+    data2 = haut_bas()
+    for i in data2:
+        print(i)
+
+        
     return render(request, "database_mode.html", {'data':data})
 
 
