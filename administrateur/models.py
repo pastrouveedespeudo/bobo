04068d7449889f2c7956379fc4bba264817edb4e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-13 19:31:56 +0200

    Update models.py

--- administrateur/models.py
+++ administrateur/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
 
-class Contact(models.Model):
+class image(models.Model):
 
-    nom = models.CharField(max_length=255)
-    couleur_haut = models.TextField()
-    couleur_bas = models.TextField()
-    couleur_cheveux = models.CharField(max_length=255)
-    image_vetement = models.ImageField(upload_to="static/bobo/")
-    image_coupe = models.ImageField(upload_to="static/bobo/")
+    
+    image = models.FileField(upload_to='bobo/')
 
+  
