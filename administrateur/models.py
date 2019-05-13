d8da2cc114e1617128f64591570d451086dc6ef9
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-13 16:17:27 +0200

    Update models.py
    bon ben jdois tout refaire

--- administrateur/models.py
+++ administrateur/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    nom = models.CharField(max_length=255)
+    couleur_haut = models.TextField()
+    couleur_bas = models.TextField()
+    couleur_cheveux = models.CharField(max_length=255)
+    image_vetement = models.ImageField(upload_to="static/bobo/")
+    image_coupe = models.ImageField(upload_to="static/bobo/")
+
