ab5a10a34b0fad5ab111e91490560c6cdb6b2fff
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-19 19:31:15 +0200

    Update urls.py

--- admin_pollu/urls.py
+++ admin_pollu/urls.py
@@ -6,4 +6,7 @@ from django.conf.urls import include
 
 urlpatterns = [
     path(r'/admin_pollu', views.admin_pollu),
+    path(r'/database', views.database),
+    path(r'/prédiction', views.prédiction),
+    path(r'/remplir_database', views.remplir_database),
 ]
