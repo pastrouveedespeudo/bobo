48103012b1a5df6e34c71d9ba81c0b6e4ecc57e2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-19 19:32:03 +0200

    Update views.py

--- admin_pollu/views.py
+++ admin_pollu/views.py
@@ -1,5 +1,33 @@
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-def admin_pollu(request):
-       
+from .database import creation_table
+
+
+def admin_pollu(request): 
+    return render(request, "admin_pollu.html")
+
+
+def database(request): 
+    return render(request, "database.html")
+
+def prédiction(request): 
+    return render(request, "prédiction.html")
+
+def remplir_database(request): 
+    return render(request, "remplir_database.html")
+
+
+
+
+def construction(request):
+
+    if request.method == "POST":
+        try:
+            creation_table()
+        except:
+            print('table déja crée')
+        
+
     return render(request, "admin_pollu.html")
