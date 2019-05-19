1b37e2e8d4894199bc9815ccf55569f35cb520c8
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-19 19:38:46 +0200

    Update météo.py

--- admin_pollu/polution/météo.py
+++ admin_pollu/polution/météo.py
@@ -11,67 +11,63 @@ from PIL import Image, ImageDraw, ImageChops
 
 
 
+def recuperation_donnée(lieu, METEO, VENT, PRESSION):
 
-class météo:
+    clé = '5a72ceae1feda40543d5844b2e04a205'
+    
+    localisation = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}".format(self.lieu,clé)
+    r = requests.get(localisation)
+    data=r.json()
 
-    def recuperation_donnée(self, lieu, METEO, VENT, PRESSION):
-        self.lieu = lieu
 
-        clé = '5a72ceae1feda40543d5844b2e04a205'
+    méteo = data['weather'][0]['main']
+    print(méteo)
+    if méteo == "Rain" or méteo == 'Thunderstorm':
+        METEO['pluie'] +=1
+    elif méteo == "Clouds" or méteo == 'Mist' or méteo == 'Haze':
+        METEO['nuageux'] +=1
+    elif méteo == "Clear":
+        METEO['beau_temps'] +=1
+
+
+    try:
+        vent_degres = data['wind']['deg']
+    except:
+        pass
+
+    
+    vent = data['wind']['speed']
+
+
+    if vent <= 3 :
+        VENT['faible'] += 1
         
-        localisation = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}".format(self.lieu,clé)
-        r = requests.get(localisation)
-        data=r.json()
+    elif vent <= 6 and vent > 3:
+        VENT['moyen fort'] += 1
+
+    elif vent <= 8 and vent > 6:
+        VENT['fort'] += 1
+
+    elif vent >= 8:
+        VENT['tres fort'] += 1
 
 
-        méteo = data['weather'][0]['main']
-        print(méteo)
-        if méteo == "Rain" or méteo == 'Thunderstorm':
-            METEO['pluie'] +=1
-        elif méteo == "Clouds" or méteo == 'Mist' or méteo == 'Haze':
-            METEO['nuageux'] +=1
-        elif méteo == "Clear":
-            METEO['beau_temps'] +=1
+    date = datetime.datetime.now()
+    mois = date.month
 
+    pression = data['main']['pressure']
 
-        try:
-            vent_degres = data['wind']['deg']
-        except:
-            pass
+    
+    if pression >= 1030:#anti
+        PRESSION['forte'] += 1
 
-        
-        vent = data['wind']['speed']
+    elif pression <= 1013:
+        PRESSION['faible'] += 1#depression
 
+    else:
+        PRESSION['normale'] += 1
 
-        if vent <= 3 :
-            VENT['faible'] += 1
-            
-        elif vent <= 6 and vent > 3:
-            VENT['moyen fort'] += 1
-
-        elif vent <= 8 and vent > 6:
-            VENT['fort'] += 1
-
-        elif vent >= 8:
-            VENT['tres fort'] += 1
-
-
-        date = datetime.datetime.now()
-        mois = date.month
-
-        pression = data['main']['pressure']
-
-        
-        if pression >= 1030:#anti
-            PRESSION['forte'] += 1
-
-        elif pression <= 1013:
-            PRESSION['faible'] += 1#depression
-
-        else:
-            PRESSION['normale'] += 1
-
-        
+    
 
 
 
