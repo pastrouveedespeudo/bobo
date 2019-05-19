3ae4ae3c58b3c17a9f1bd1d41206bb70468c1300
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-19 19:48:24 +0200

    Update main.py

--- admin_pollu/polution/main.py
+++ admin_pollu/polution/main.py
@@ -41,159 +41,159 @@ from database import insertion_table
 from database import visualisation_table
 
 
-class main:
+
+
+
+def météologie():
+
+    referentiel = date_heure()
 
     
-    def météologie(self):
+    for i in LIST_CITY:
+        print(i)
 
-        referentiel = date_heure()
-
+        recuperation_donnée(i, WEATHER, WIND, PRESSURE)
         
-        for i in LIST_CITY:
-            print(i)
+        données = display_dict(PRESSURE, WEATHER, WIND)
+        print(données)
+        insertion_table.insertion_meteo(self, i, referentiel[0],#ICI
+                                        referentiel[1],
+                                        données[0], données[1],
+                                        données[2])
 
-            météo.recuperation_donnée(self, i, WEATHER, WIND, PRESSURE)
-            
-            données = display_dict(PRESSURE, WEATHER, WIND)
-            print(données)
-            insertion_table.insertion_meteo(self, i, referentiel[0],
-                                            referentiel[1],
-                                            données[0], données[1],
-                                            données[2])
+        raise_dict(PRESSURE, WEATHER, WIND)
+        
 
-            raise_dict(PRESSURE, WEATHER, WIND)
-            
+def climat():
 
-    def climat(self):
+    referentiel = date_heure()
 
-        referentiel = date_heure()
     
+    for i in LIST_CITY:
+        print(i)
+
+        climat.recuperation_donnée(self, i, CLIMAT)
+        climat.saison(self, SAISON)
         
-        for i in LIST_CITY:
-            print(i)
-
-            climat.recuperation_donnée(self, i, CLIMAT)
-            climat.saison(self, SAISON)
-            
-            données = display_dict(CLIMAT, SAISON)
-            
-            insertion_table.insertion_climat(self, données[0], données[1],
-                                             referentiel[0],referentiel[1], i)
-            
-            raise_dict(CLIMAT, SAISON)   
+        données = display_dict(CLIMAT, SAISON)
+        
+        insertion_table.insertion_climat(self, données[0], données[1],#ICI
+                                         referentiel[0],referentiel[1], i)
+        
+        raise_dict(CLIMAT, SAISON)   
 
 
-    def pollution(self):
+def pollution():
 
-        referentiel = date_heure()
+    referentiel = date_heure()
 
-        for i in LIST_CITY:
-            print(i)
-            
-            particule.france(self, i, VILLE_POLLUE2018)
-            particule.industrie(self, i, REGION_INDUSTRIEL_POLLUEE)
+    for i in LIST_CITY:
+        print(i)
+        
+        france(self, i, VILLE_POLLUE2018)
+        industrie(self, i, REGION_INDUSTRIEL_POLLUEE)
 
-            données = display_dict(VILLE_POLLUE2018,
-                         REGION_INDUSTRIEL_POLLUEE)
+        données = display_dict(VILLE_POLLUE2018,
+                     REGION_INDUSTRIEL_POLLUEE)
 
-            insertion_table.insertion_polution(self, données[0], données[1],
-                                               referentiel[0],referentiel[1], i)
-            
-            raise_dict(VILLE_POLLUE2018,
-                       REGION_INDUSTRIEL_POLLUEE)
+        insertion_table.insertion_polution(self, données[0], données[1],#ICI
+                                           referentiel[0],referentiel[1], i)
+        
+        raise_dict(VILLE_POLLUE2018,
+                   REGION_INDUSTRIEL_POLLUEE)
 
 
-    def sociologie(self):
+def sociologie():
 
-        referentiel = date_heure()
+    referentiel = date_heure()
+
+    
+    for i in LIST_CITY:
+        print(i)
+        
+        habitant(self, i, POPULATION_ACTIVE_HABITANT)
+    
+        données = display_dict(POPULATION_ACTIVE_HABITANT)
+
+        insertion_table.insertion_sociologie(self, données[0], referentiel[0],#ICI
+                                            referentiel[1], i)
+         
+        raise_dict(POPULATION_ACTIVE_HABITANT)
+
+
+def trafic_routier():
+
+    referentiel = date_heure()
+
+    for i in LIST_CITY:
+        print(i)
+
+        trafique_circulation(TRAFIQUE, HEURE)
+        habitude(POINTE, WEEKEND)
+        bouchons(i, BOUCHON)
+        activité_execptionnelle(i, ACTIVITE_EXEPTIONNELLE)
+        
+        données =  display_dict(TRAFIQUE, HEURE, POINTE, WEEKEND, BOUCHON,
+                     ACTIVITE_EXEPTIONNELLE)
+        print(données)
+
+
+        insertion_table.insertion_trafic_routier(self, données[0], données[1],#ICI#ICI#ICI
+                                                 données[2], données[3], données[4], données[5],
+                                                 referentiel[0], referentiel[1], i)
+
+                    
+        raise_dict(TRAFIQUE, HEURE, POINTE, WEEKEND, BOUCHON,
+                   ACTIVITE_EXEPTIONNELLE)
+
+
+def particule_plage():
+
+    referentiel = date_heure()
+
+    print('\n')
+    for i in LIST_CITY:
+        print(i) 
+
+        particule(i, PARTICULE_PLAGE)
+        
+        données = display_dict(PARTICULE_PLAGE)
+
+        insertion_table.insertion_particule_plage(self, données[0], referentiel[0],#ICI#ICI
+                                            referentiel[1], i)
+        
+        raise_dict(PARTICULE_PLAGE)
+
+
+def particule():
+
+    referentiel = date_heure()
+
+    print('\n')
+    for i in LIST_CITY:
+        print(i) 
+
+        particule2(self,i, PARTICULE)
+        
+        données = display_dict_particule(PARTICULE)
+
+        insertion_table.insertion_particule(self, données[0], referentiel[0],#ICI#ICI
+                                            referentiel[1], i)
+        
+        raise_dict(PARTICULE)
 
         
-        for i in LIST_CITY:
-            print(i)
-            
-            socio.habitant(self, i, POPULATION_ACTIVE_HABITANT)
-        
-            données = display_dict(POPULATION_ACTIVE_HABITANT)
-
-            insertion_table.insertion_sociologie(self, données[0], referentiel[0],
-                                                referentiel[1], i)
-             
-            raise_dict(POPULATION_ACTIVE_HABITANT)
 
 
-    def trafic_routier(self):
 
-        referentiel = date_heure()
-
-        for i in LIST_CITY:
-            print(i)
-
-            trafique.trafique_circulation(self, TRAFIQUE, HEURE)
-            trafique.habitude(self, POINTE, WEEKEND)
-            trafique.bouchons(self, i, BOUCHON)
-            trafique.activité_execptionnelle(self, i, ACTIVITE_EXEPTIONNELLE)
-            
-            données =  display_dict(TRAFIQUE, HEURE, POINTE, WEEKEND, BOUCHON,
-                         ACTIVITE_EXEPTIONNELLE)
-            print(données)
-
-
-            insertion_table.insertion_trafic_routier(self, données[0], données[1],
-                                                     données[2], données[3], données[4], données[5],
-                                                     referentiel[0], referentiel[1], i)
-
-                        
-            raise_dict(TRAFIQUE, HEURE, POINTE, WEEKEND, BOUCHON,
-                       ACTIVITE_EXEPTIONNELLE)
-
-
-    def particule_plage(self):
-
-        referentiel = date_heure()
-
-        print('\n')
-        for i in LIST_CITY:
-            print(i) 
-
-            particule.particule(self,i, PARTICULE_PLAGE)
-            
-            données = display_dict(PARTICULE_PLAGE)
-
-            insertion_table.insertion_particule_plage(self, données[0], referentiel[0],
-                                                referentiel[1], i)
-            
-            raise_dict(PARTICULE_PLAGE)
-
-
-    def particule(self):
-
-        referentiel = date_heure()
-
-        print('\n')
-        for i in LIST_CITY:
-            print(i) 
-
-            particule.particule2(self,i, PARTICULE)
-            
-            données = display_dict_particule(PARTICULE)
-
-            insertion_table.insertion_particule(self, données[0], referentiel[0],
-                                                referentiel[1], i)
-            
-            raise_dict(PARTICULE)
-
-            
-
-if __name__ == '__main__':
-
-    main = main()
-    main.météologie()
-    main.climat()
-    main.pollution()
-    main.sociologie()
-    main.trafic_routier()
-    main.particule()
-    main.particule_plage()
+main = main()
+main.météologie()
+main.climat()
+main.pollution()
+main.sociologie()
+main.trafic_routier()
+main.particule()
+main.particule_plage()
 
 
 
