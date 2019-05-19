34aed03e6436d7d283dba0a1d1922e2f1f727260
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-19 22:38:11 +0200

    Update views.py

--- admin_pollu/views.py
+++ admin_pollu/views.py
@@ -5,6 +5,48 @@ from django.shortcuts import render
 from .database import creation_table
 
 
+
+
+
+def remplir_database(request):
+
+    if request.method == "POST":
+        print('ouiiiiiiiiiiiii')
+        remplir = request.POST.get('remplissage')
+        verif = request.POST.get('verification')
+        delete = request.POST.get('delete')
+
+
+        
+        print(remplir)
+
+        if remplir:
+            pass
+        if verif:
+            print('ouiiiiiiiii')
+
+        if delete:
+            print('deeeeeeeeelte')
+
+        
+    return render(request, "remplir_database.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def admin_pollu(request): 
     return render(request, "admin_pollu.html")
 
@@ -15,15 +57,13 @@ def database(request):
 def prédiction(request): 
     return render(request, "prédiction.html")
 
-def remplir_database(request): 
-    return render(request, "remplir_database.html")
-
 
 
 
 def construction(request):
 
     if request.method == "POST":
+        print('ouiiiiiiiiiiiiiiiiiiiiiiiii')
         try:
             creation_table()
         except:
