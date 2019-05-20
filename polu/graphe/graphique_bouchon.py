bea1ede300a5cb4993b2e913e7d9547145756771
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-20 19:54:52 +0200

    Update graphique_bouchon.py
    un crochet 1h

--- polu/graphe/graphique_bouchon.py
+++ polu/graphe/graphique_bouchon.py
@@ -4,7 +4,9 @@ import pylab
 import psycopg2
 import numpy as np
 
-from polu_ana.polution.database2 import clean_data
+
+from fonction_graphe import moyenne
+
 
 def visu(ville):
     
@@ -26,3 +28,127 @@ def visu(ville):
     liste = [i for i in rows]
 
     return liste
+
+def traitement_bouchon(donnée):
+    liste = ['non', 'petit', 'moyen', 'grand', 'assez grand',
+             'tres grand']
+    non = []
+    petit = [0]
+    moyen = [0]
+    grand = []
+    assez_grand = []
+    tres_grand = []
+    
+
+    for i in donnée:
+        if i[0] == 'non':
+            non.append(int(i[1]))
+        elif i[0] == 'petit':
+            petit.append(int(i[1]))
+        elif i[0] == 'moyen':
+            moyen.append(int(i[1]))  
+        elif i[0] == 'grand':
+            grand.append(int(i[1]))
+        elif i[0] == 'assez grand':
+            assez_grand.append(int(i[1]))
+        elif i[0] == 'tres grand':
+            tres_grand.append(int(i[1]))
+
+    #print(non, petit, moyen, grand, assez_grand, tres_grand)
+
+
+    donnée_non = moyenne(non)
+
+    donnée_petit = moyenne(petit)
+
+    donnée_moyen = moyenne(moyen)
+
+    donnée_grand = moyenne(grand)
+
+    donnée_assez_grand = moyenne(assez_grand)
+
+    donnée_tres_grand = moyenne(tres_grand)
+
+    return donnée_non[0], donnée_petit[0], donnée_moyen[0],\
+            donnée_grand[0], donnée_assez_grand[0],\
+            donnée_tres_grand[0],\
+            donnée_non[1], donnée_petit[1], donnée_moyen[1],\
+            donnée_grand[1], donnée_assez_grand[1],\
+            donnée_tres_grand[1]
+
+
+
+
+def diagramme(donnée_non, donnée_petit, donnée_moyen,
+              donnée_grand, donnée_assez_grand,
+              donnée_tres_grand,
+              er_non, er_petit, er_moyen,
+              er_grand, er_assez_grand,
+              er_tres_grand):
+
+    
+    plt.bar(range(6), [donnée_non, donnée_petit, donnée_moyen,
+                       donnée_grand, donnée_assez_grand,
+                        donnée_tres_grand],
+                        width = 0.1, color = 'red',
+                       yerr = [er_non, er_petit, er_moyen,
+                              er_grand, er_assez_grand,
+                              er_tres_grand],
+                        ecolor = 'black', capsize = 10)
+                
+
+
+    plt.xticks(range(6), ['non', 'petit', 'moyen',
+                          'grand', 'assez grand', 'tres grand'])
+
+        
+    plt.ylabel('Taux de pollution en AQI')
+    plt.title("Taux de pollution selon les bouchons")
+    
+    plt.show()
+
+
+
+
+
+
+
+        
+a = visu('lyon')
+donnée = traitement_bouchon(a)
+
+diagramme(donnée[0], donnée[1], donnée[2], donnée[3], donnée[4],
+          donnée[5], donnée[6], donnée[7], donnée[8], donnée[9],
+          donnée[10], donnée[11])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
