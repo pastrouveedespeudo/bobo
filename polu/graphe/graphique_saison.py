454d809b98ce43f7e8612275a91dc0eb0214d492
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-20 21:38:51 +0200

    Update graphique_saison.py

--- polu/graphe/graphique_saison.py
+++ polu/graphe/graphique_saison.py
@@ -4,7 +4,7 @@ import pylab
 import psycopg2
 import numpy as np
 
-from polu_ana.polution.database2 import clean_data
+from fonction_graphe import moyenne
 
 def visu(ville):
     
@@ -26,3 +26,82 @@ def visu(ville):
     liste = [i for i in rows]
 
     return liste
+
+
+def traitement_saison(donnée):
+
+    primtemps = []
+    été = [0]
+    hiver = [0]
+    automne = [0]
+
+    for i in donnée:
+  
+        if i[0] == 'primtemps':
+            primtemps.append(int(i[1]))
+        elif i[0] == 'été':
+            été.append(int(i[1]))
+        elif i[0] == 'hiver':
+            hiver.append(int(i[1]))  
+        elif i[0] == 'automne':
+            automne.append(int(i[1])) 
+
+
+    donnée_primtemps = moyenne(primtemps)
+    donnée_été = moyenne(été)
+    donnée_hiver = moyenne(hiver)
+    donnée_automne = moyenne(automne)
+
+
+
+    return donnée_primtemps[0], donnée_été[0], donnée_hiver[0],\
+            donnée_automne[0], donnée_primtemps[1], donnée_été[1],\
+            donnée_hiver[1], donnée_automne[1]
+
+
+def diagramme(donnée_primtemps, donnée_été, donnée_hiver, donnée_automne,
+              er_primtemps, er_été, er_hiver, er_automne):
+
+
+    plt.bar(range(4), [donnée_primtemps, donnée_été, donnée_hiver,
+                       donnée_automne],
+                        width = 0.1, color = 'red',
+                       yerr = [er_primtemps, er_primtemps, er_été,
+                              er_automne],
+                        ecolor = 'black', capsize = 10)
+                
+
+
+    plt.xticks(range(4), ['primtemps', 'été', 'hiver',
+                          'automne'])
+
+        
+    plt.ylabel('Taux de pollution en AQI')
+    plt.title("Taux de pollution selon les saisons")
+    
+    plt.show()
+
+
+a = visu('lyon')
+donnée = traitement_saison(a)
+
+diagramme(donnée[0], donnée[1], donnée[2], donnée[3], donnée[4],
+          donnée[5], donnée[6], donnée[7])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
