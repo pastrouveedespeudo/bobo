f62f1a1993e2733251570058f4ac095b6df236da
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-21 23:39:17 +0200

    Update suppresion.py
    auto

--- admin_pollu/digibot/suppresion.py
+++ admin_pollu/digibot/suppresion.py
@@ -1,25 +1,55 @@
 import psycopg2
 
+def un():
+    conn = psycopg2.connect(database='bobo',
+                            user='postgres',
+                            host='127.0.0.1',
+                            password='tiotiotio333')  
 
-conn = psycopg2.connect(database='bobo',
-                        user='postgres',
-                        host='127.0.0.1',
-                        password='tiotiotio333')  
-
-cursor = conn.cursor()
-
-
-cursor.execute("""delete from ville where (bouchon='non_weekend and
-                bouchon='weekend'""")
+    cursor = conn.cursor()
 
 
-conn.commit()
+    ##cursor.execute("""delete from ville where (bouchon='non_weekend and
+    ##                bouchon='weekend'""")
+
+    cursor.execute("""SELECT POINTE, HEURE particule FROM ville;""")
+
+    conn.commit()
 
 
 
-rows = cursor.fetchall()
-liste = [i for i in rows]
+##    rows = cursor.fetchall()
+##    liste = [i for i in rows]
+##
+##    for i in liste:
+##        print(i)
+##        print('\n')
 
-for i in liste:
-    print(i)
-    print('\n')
+def deux():
+    conn = psycopg2.connect(database='bobo',
+                            user='postgres',
+                            host='127.0.0.1',
+                            password='tiotiotio333')  
+
+    cursor = conn.cursor()
+
+
+    ##cursor.execute("""delete from ville where (bouchon='non_weekend and
+    ##                bouchon='weekend'""")
+
+    cursor.execute("""alter table ville drop POINTE;""")
+
+    conn.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+un()
