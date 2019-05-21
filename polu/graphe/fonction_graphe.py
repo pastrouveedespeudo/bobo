532da1472b96929c06d67c75aaf84bd1b4faf524
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-21 22:54:40 +0200

    Update fonction_graphe.py

--- polu/graphe/fonction_graphe.py
+++ polu/graphe/fonction_graphe.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def moyenne(liste):
-    #print(liste, len(liste))
-    moyenne = int(sum(liste)) / int(len(liste))
-
+    try:
+        moyenne = int(sum(liste)) / int(len(liste))
+    except:
+        moyenne = 0
     variance = np.var(liste)
 
     erreur = (variance/len(liste))**(1/2)
-    #print(moyenne, erreur)
     return moyenne, erreur
 
