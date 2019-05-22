796f28379256c6fca285c4ead22c0c70d656f51d
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-22 11:19:57 +0200

    Update fonction_graphe.py

--- polu/graphe/fonction_graphe.py
+++ polu/graphe/fonction_graphe.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+
 
 def moyenne(liste):
     try:
@@ -10,3 +12,24 @@ def moyenne(liste):
     erreur = (variance/len(liste))**(1/2)
     return moyenne, erreur
 
+
+
+def new():
+
+    liste = os.listdir(r'C:\Users\jeanbaptiste\bobo\bobo\static\popo')
+
+    try:
+        nb = str((liste[-1][-3])) + str((liste[-1][-4])) + str((liste[-1][-5]))
+        nb = int(nb) + 1
+    except:
+        pass
+    
+    try:
+        nb = str((liste[-1][-4])) + str((liste[-1][-5]))
+        nb = int(nb) + 1
+    except:
+        nb = int(liste[-1][-5]) + 1
+        
+    new_save = str(liste[-1][:-5]) + str(nb) + '.png' 
+
+    return new_save
