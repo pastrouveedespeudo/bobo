a6f827bc231f8bf314f5aa66b949102fbb2d8d19
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-22 14:54:31 +0200

    Update views.py

--- admin_pollu/views.py
+++ admin_pollu/views.py
@@ -1,301 +1,166 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
+from django.shortcuts import render
 
-import requests
-import urllib.request
-import datetime
-from bs4 import *
+from .polution.database2 import creation_table
+from .polution.database2 import clean_data
+from .polution.database2 import clean_data2
+from .polution.database2 import clean_data3
+from .polution.database2 import clean_data4
+from .polution.database2 import visualisation
 
-from .donnée_site.pollution import taux_particule
-from .donnée_site.pollution import temps_ville
-from .donnée_site.pollution import climat_ville
-from .donnée_site.pollution import saison
-from .donnée_site.pollution import traffique
-from .donnée_site.pollution import habitude
-from .donnée_site.pollution import ville_pollué_classement
-from .donnée_site.pollution import region_industrielle
-from .donnée_site.pollution import pression_ville
-from .donnée_site.pollution import activité_execptionnelle
-from .donnée_site.pollution import socio
-from .donnée_site.pollution import bouchons
+from .polution.main import météologie
+from .polution.main import climat
+from .polution.main import pollution
+from .polution.main import sociologie
+from .polution.main import trafic_routier
+from .polution.main import particule_plage
+from .polution.main import particulee
 
-from.polu_ana.polution.traitement_de_donnée import recuperation_data
-from.polu_ana.polution.traitement_de_donnée import condition
+from .polution.traitement_de_donnée import condition
+from .polution.traitement_de_donnée import recuperation_data
 
 
-def home(request):
-    return render(request, 'home.html')
+def remplir_database(request):
 
-
-def polution(request):
-
-    return render(request, 'polution.html')
-
-
-
-def charger(request):
-    return render(request, 'charger.html')
-
-def donnée(request):
-
-    data_lyon = taux_particule('lyon')
-    data_paris = taux_particule('paris')
-    data_marseille = taux_particule('marseille')
-
-
-
-    météo_lyon = temps_ville('lyon', 'météo')
-    vent_lyon = temps_ville('lyon', 'vent')
-
-    météo_paris = temps_ville('paris', 'météo')
-    vent_paris = temps_ville('paris', 'vent')
-
-    météo_marseille = temps_ville('marseille', 'météo')
-    vent_marseille = temps_ville('marseille', 'vent')
-
-    température_lyon = climat_ville('lyon')
-    température_paris = climat_ville('paris')
-    température_marseille = climat_ville('marseille')
-
-    saison_actuelle =  saison()
-
-    traffique_lyon = traffique('lyon')
-    départ_routier_lyon = traffique_lyon[0] 
-    heure_pointe_lyon = traffique_lyon[1] 
-    regulier_jour_lyon = traffique_lyon[2] 
-    non_pointe_lyon = traffique_lyon[3] 
-
-
-    traffique_paris = traffique('paris')
-    départ_routier_paris = traffique_paris[0] 
-    heure_pointe_paris = traffique_paris[1] 
-    regulier_jour_paris = traffique_paris[2] 
-    non_pointe_paris = traffique_paris[3] 
-
-
-    
-    traffique_marseille = traffique('marseille')
-    départ_routier_marseille = traffique_marseille[0] 
-    heure_pointe_marseille = traffique_marseille[1] 
-    regulier_jour_marseille = traffique_marseille[2] 
-    non_pointe_marseille = traffique_marseille[3] 
-
-    jour = habitude()
-
-    classement_lyon = ville_pollué_classement('lyon')
-    classement_paris = ville_pollué_classement('paris')
-    classement_marseille = ville_pollué_classement('marseille')
-
-    pole_lyon = region_industrielle('lyon')
-    pole_paris = region_industrielle('paris')
-    pole_marseille = region_industrielle('marseille')
-
-    pression_lyon = pression_ville('lyon')
-    pression_paris = pression_ville('paris')
-    pression_marseille = pression_ville('marseille')
-
-    manif_lyon = activité_execptionnelle('lyon')
-    manif_paris = activité_execptionnelle('paris')
-    manif_marseille = activité_execptionnelle('marseille')
-    
-
-    socio_lyon = socio('lyon')
-    socio_paris = socio('paris')
-    socio_marseille = socio('marseille')
-
-    bouchons_lyon = bouchons('lyon')
-    bouchons_paris = bouchons('paris')
-
-
-
-
-
-
-
-
-
-    return render(request, 'donnée.html', {'lyon':data_lyon,
-                                           'paris':data_paris,
-                                           'marseille':data_marseille,
-                                           'météo_lyon':météo_lyon,
-                                           'météo_marseille':météo_marseille,
-                                           'météo_paris':météo_paris,
-                                           'vent_lyon':vent_lyon,
-                                           'vent_paris':vent_paris,
-                                           'vent_marseille':vent_marseille,
-                                           'température_lyon':round(température_lyon),
-                                           'température_paris':round(température_paris),
-                                           'température_marseille':round(température_marseille),
-                                           'saison_actuelle':saison_actuelle,
-                                           'départ_routier_lyon':départ_routier_lyon,
-                                           'heure_pointe_lyon':heure_pointe_lyon,
-                                           'regulier_jour_lyon':regulier_jour_lyon,
-                                           'non_pointe_lyon':non_pointe_lyon,
-                                           'départ_routier_paris':départ_routier_paris,
-                                           'heure_pointe_paris':heure_pointe_paris,
-                                           'regulier_jour_marseille':regulier_jour_marseille,
-                                           'non_pointe_marseille':non_pointe_marseille,
-                                           'départ_routier_paris':départ_routier_paris,
-                                           'heure_pointe_paris':heure_pointe_paris,
-                                           'regulier_jour_paris':regulier_jour_paris,
-                                           'non_pointe_paris':non_pointe_paris,
-                                           'weekend':jour[0],
-                                           'jour_semaine':jour[1],
-                                           'classement_lyon':classement_lyon,
-                                           'classement_paris':classement_paris,
-                                           'classement_marseille':classement_marseille,
-                                           'pole_lyon':pole_lyon,
-                                           'pole_paris':pole_paris,
-                                           'pole_marseille':pole_marseille,
-                                           'pression_lyon':pression_lyon,
-                                           'pression_paris':pression_paris,
-                                           'pression_marseille':pression_marseille,
-                                           'manif_lyon':manif_lyon,
-                                           'manif_paris':manif_paris,
-                                           'manif_marseille':manif_marseille,
-                                           'socio_lyon':socio_lyon,
-                                           'socio_marseille':socio_marseille,
-                                           'socio_paris':socio_paris,
-                                           'bouchons_lyon':bouchons_lyon,
-                                           'bouchons_paris':bouchons_paris})
-
-
-    return render(request, 'donnée.html')
-
-
-
-def info_pollu(request):
-    return render(request, 'info_pollu.html')
-
-
-
-
-def machine_a_o(request):
-    return render(request, 'machine a o.html')
-
-def prediction(request):
     if request.method == "POST":
-        lyon = request.POST.get('lyon')
-        paris = request.POST.get('paris')
-        marseille = request.POST.get('marseille')
+        print('ouiiiiiiiiiiiii')
+        remplir = request.POST.get('remplissage')
+        verif = request.POST.get('verification')
+        delete = request.POST.get('delete')
 
-        if lyon:
-            donnée = recuperation_data('lyon')
-            pred = condition(donnée[0], donnée[1], donnée[2],
-                    donnée[3], donnée[4], donnée[5], 'lyon')
-            return HttpResponse(pred)
+
+        
+        print(remplir)
+
+        if remplir:
+            
+            météologie()
+            climat()       
+            pollution()
+            sociologie()
+            trafic_routier()
+            particulee()
+            particule_plage()
+
+            data = {'data':'fin data rempli'}
+
+
+        if verif:
+            clean_data()
+            print('ouiiiiiiiii')
+
+
+        
+    return render(request, "remplir_database.html")
+
+
+
+def admin_pollu(request): 
+    return render(request, "admin_pollu.html")
+
+
+def database(request):
+    
+    if request.method == "POST":
+        print('ouiiiiiiiiiiiii')
+        clean_data()
+        clean_data2()
+        clean_data3()
+        clean_data4()
+        paris = request.POST.get('paris')
+        lyon = request.POST.get('lyon')
+        marseille = request.POST.get('marseille')
+        print(marseille, paris, lyon)
+
         
         if marseille:
-            donnée = recuperation_data('marseille')
-            pred = condition(donnée[0], donnée[1], donnée[2],
-                    donnée[3], donnée[4], donnée[5], 'marseille')
-            print(pred,'0000000000000000000000000000000000000000000000')
-            return HttpResponse(pred)
+            data = visualisation('marseille')
+            liste = []
+
+            for i in data:
+                liste.append(i)
+                liste.append('<br><br>')
+                
+            return HttpResponse(liste)
+
+        
+        if lyon:
+            data = visualisation('lyon')
+            liste = []
+
+            for i in data:
+                liste.append(i)
+                liste.append('<br><br>')
+                
+            return HttpResponse(liste)
+
         
         if paris:
-            donnée = recuperation_data('paris')
-            pred = condition(donnée[0], donnée[1], donnée[2],
-                    donnée[3], donnée[4], donnée[5], 'paris')
+            data = visualisation('paris')
+            liste = []
 
-            return HttpResponse(pred)
-    
-    return render(request, 'prediction.html')
+            for i in data:
+                liste.append(i)
+                liste.append('<br><br>')
+                
+            return HttpResponse(liste)
 
-
-
-def polution_lyon(request):
-
-
-    a = heure()
-    b = temps("lyon")
-    c = particule("lyon")
-    
-
-    return render(request, 'polution_lyon.html', {'heure':a[0],'minute':a[1], 'temps':b, 'pollution':c})
-
-
-
-def polution_marseille(request):
-
-    a = heure()
-    b = temps("marseille")
-    c = particule("marseille")
-
-    
-    return render(request, 'polution_marseille.html', {'heure':a[0],'minute':a[1], 'temps':b, 'pollution':c})
-
-def polution_paris(request):
-
-    a = heure()
-    b = temps("paris")
-    c = particule("paris")
-    
-    return render(request, 'polution_paris.html', {'heure':a[0],'minute':a[1], 'temps':b, 'pollution':c})
-
-
-def graphique(request):
-    return render(request, 'graphique.html')
-
-
-
-
-def heure():
-
-    date = datetime.datetime.now()
-
-    heure = date.hour
-    minute = date.minute
-
-    return heure, minute
-
-
-
-
-
-def temps(lieu):
-
-    clé = '5a72ceae1feda40543d5844b2e04a205'
         
-    localisation = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}".format(lieu,clé)
-
-    r = requests.get(localisation)
-
-    data=r.json()
-    
-    méteo = data['weather'][0]['main']
-
-    if méteo == "Clear":
-        méteo = "Beau"
-
-    elif méteo == "Clouds":
-        méteo = "Nuageux"
-    return méteo
+    return render(request, "database.html")
 
 
-def particule(lieu):
 
 
-    path = "https://air.plumelabs.com/fr/live/{}".format(lieu)
 
-    r = requests.get(path)
+def prédiction(request):
+    if request.method == "POST":
+        print('coucouuuuuuuuuuu')
+        
+        prédiction = request.POST.get('prediction')
+        if prédiction:
+            clean_data()
+            clean_data2()
+            clean_data3()
+            clean_data4()
+            predi = []
+            liste = ['paris', 'lyon', 'marseille']
+            
+            for i in liste:
+                print(i)
+                donnée = recuperation_data(i)
+            
+                pred = condition(donnée[0], donnée[1], donnée[2],
+                        donnée[3], donnée[4], donnée[5], i)
+                predi.append((i, pred))
+            
+            return HttpResponse(predi)
+            
+                
+    return render(request, "prédiction.html")
 
 
-    page = r.content
-    soup = BeautifulSoup(page, "html.parser")
 
-    liste = []
-    propriete = soup.find_all("div")
-    for i in propriete:
-        liste.append(i.get_text())
 
-    phrase_clé = "a atteint un niveau élevé de pollution. Supérieur à la limite maximum pour 24h établie par l'OMS"
-    
-    recherche_taux = str(liste).find(str(phrase_clé))
-    liste_epluché = liste[20:21]
-    polution = liste_epluché[0][31:34]
+def construction(request):
 
-    return polution
-    
+    if request.method == "POST":
+        print('ouiiiiiiiiiiiiiiiiiiiiiiiii')
+        try:
+            creation_table()
+        except:
+            print('table déja crée')
+        
+
+    return render(request, "admin_pollu.html")
+
+
+
+
+
+
+
+
 
 
 
