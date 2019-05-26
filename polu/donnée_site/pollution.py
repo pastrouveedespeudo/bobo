40282bcb5f8203d540640a75a6c765e84e878411
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-26 12:53:12 +0200

    Update pollution.py

--- polu/donnée_site/pollution.py
+++ polu/donnée_site/pollution.py
@@ -121,9 +121,8 @@ def traffique(lieu):
     heure = date.hour
     minute = date.minute
 
-
-
     heure = heure + 2
+
     heure_pointe_semaine = [7,8,9,16,17,18,19]
 
     départ_routier = [(2,1), (5,1), (9,2), (16,2), (22,2),(23,2),
