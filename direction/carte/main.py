d8ba81bd8ff8debcd2ce773d73f5e1823b2726c6
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-27 20:09:07 +0200

    Update main.py
    8m en 8m c un peu chiant en vrai, et des que ca change de ville pritn() au pire

--- direction/carte/main.py
+++ direction/carte/main.py
@@ -8,21 +8,72 @@ from direction.vent import le_vent
 
 liste = ['crest']
 
-for i in liste:
-    print('ville cherchée:', i + '\n')
 
-    lat, long = ville(i)
-    print('de lattitude:', lat, 'de longitude', long, '\n')
+new_lat = ''
+new_long = ''
 
-    adresse = par_lat_par_long(lat, long)
-    print('adresse trouvée :', adresse, '\n')
 
-    vent, degres_vent = le_vent(i)
 
-    print('vent de :', vent, 'de degres :', degres_vent, '\n')
 
-    position_vent = calcul_vent(degres_vent)
-    print('donc le vent va vers:', position_vent, '\n')
 
-    nouvelle_position = long_lat(lat, long, vent, position_vent)
-    print('la nouvelle position est de :', nouvelle_position)    
+while True:
+
+    for i in liste:
+        
+        if new_lat != '' and new_long != '':
+            
+            print('de lattitude:', new_lat, 'et de longitude', new_long)
+            
+            adresse = par_lat_par_long(lat, long)
+            adresse = adresse.split()
+
+
+            lat, long = ville(adresse[-6][:-1])
+            print('adresse trouvée :', adresse[-6][:-1], '\n')
+            vent, degres_vent = le_vent(adresse[-6][:-1])
+            
+        else:
+            lat, long = ville(i)
+            print('de lattitude:', lat, 'de longitude', long, '\n')
+            vent, degres_vent = le_vent(i)
+
+
+
+        
+        #print('vent de :', vent, 'de degres :', degres_vent, '\n')
+            
+        position_vent = calcul_vent(degres_vent)
+        #print('donc le vent va vers:', position_vent, '\n')
+        if new_lat != '' and new_long != '':
+            nouvelle_position = long_lat(new_lat, new_long, vent, position_vent)
+            print('la nouvelle position est de :', nouvelle_position)    
+        else:
+            nouvelle_position = long_lat(lat, long, vent, position_vent)
+            print('la nouvelle position est de :', nouvelle_position)    
+        
+        new_lat = nouvelle_position[0]
+        new_long = nouvelle_position[1]
+
+        
+        print('\n')
+        print('\n')
+        print('\n')
+        print('\n')
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
