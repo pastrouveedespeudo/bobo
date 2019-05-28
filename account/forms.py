c65ab0dabc91faf61b049cb25a5132a19062925b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-28 15:14:13 +0200

    Update forms.py
    ok faut voir ca comme du ménage, en vrai j'en ai chié avec ce truk vent, enfete ca fait du bien de ranger

--- account/forms.py
+++ account/forms.py
@@ -1,13 +1,10 @@
 """Here we create fonctions for login and registration"""
 
 from django import forms
-from django.contrib.auth import (
-    authenticate,
-    get_user_model
-    )
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
 
 class UserLoginForm(forms.Form):
     """this is form for login and run session"""
@@ -38,29 +35,29 @@ class UserRegisterForm(forms.ModelForm):
     username = forms.CharField()
     email = forms.EmailField()
     email2 = forms.EmailField()
-
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         """We call username email and password from meta class"""
 
         model = User
-        fields = [
-            'username',
-            'email',
-            'email2',
-            'password',
-            ]
+        
+        fields = ['username',
+                 'email',
+                 'email2',
+                 'password']
 
     def clean(self, *args, **kwargs):
         """We cleanning it"""
 
         email = self.cleaned_data.get('email')
         email2 = self.cleaned_data.get('email2')
+        
         if email != email2:
             raise forms.ValidationError("pas les meme email")
 
         email_qs = User.objects.filter(email=email)
+        
         if email_qs.exists():
             raise forms.ValidationError(
                 "email existe deja")
