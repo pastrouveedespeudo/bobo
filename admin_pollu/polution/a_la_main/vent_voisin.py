25c957f6e4c4211a1f57c9d96c77ab51d92d3732
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-28 21:58:54 +0200

    Update vent_voisin.py

--- admin_pollu/polution/a_la_main/vent_voisin.py
+++ admin_pollu/polution/a_la_main/vent_voisin.py
@@ -183,59 +183,103 @@ def traitement_ville(liste, liste1, nom_liste):
 
     c = 0
     for i in liste:
-        print(i)
+     
         vent = vent_voisin(str(liste1[c]), i, nom_liste)
-        print(vent)
         liste2.append((i, vent))
         
         c+=1
-        
+
+
+
     return liste2
+
+
+
+def apport_pollu_vent(vent, liste_dir):
+
+    for i in liste_dir:
+        if vent == i:
+            return True
+
+
+def traitement_liste(liste, liste_ville):
+
+    liste1 = []
+
+    for i in liste:
+        a = apport_pollu_vent(i[1], liste_ville)
+        if a == True:
+            liste1.append(i[0])
+
+    return liste1
+
+
+
+def traitement_liste_France(liste, liste_ville):
+
+    liste1 = []
+
+    for i in liste:
+ 
+        if i[0] == i: 
+            a = apport_pollu_vent(i[1], direction_ville4)
+            if a == True:
+                liste1.append(i[0])
+            
+        else:
+            a = apport_pollu_vent(i[1], direction_ville44)
+            
+            if a == True:
+                liste1.append(i[0])
+
+    return liste1
+
 
     
 def apport_pollu():
+
+    liste = []
     
     a = traitement_ville(ville_haut_droite, num_ville1, 'ville_haut_droite')
-    b = traitement_ville(ville_haut_gauche, num_ville, 'ville_haut_gauche')
-    c = traitement_ville(ville_haut_droite2, num_ville2, 'ville_haut_droite2')
-    d =traitement_ville(ville_haut_droite3, num_ville3, 'ville_haut_droite3')
-    e =traitement_ville(ville_haut_droite4, num_ville4, 'ville_haut_droite4')
-    f =traitement_ville(bas_droite6, num_ville6, 'bas_droite6')
-    g =traitement_ville(ville_bas_gauche9, num_ville9, 'ville_bas_gauche9')
-    h =traitement_ville(ville_haut_droite5, num_ville5, 'ville_haut_droite5')
-    i =traitement_ville(ville_bas_gauche8, num_ville8, 'ville_bas_gauche8')
-    j =traitement_ville(ville_bas_droite7, num_ville7, 'ville_bas_droite7')
+    aa = traitement_liste(a, direction_ville1)
     
+    
+    b = traitement_ville(ville_haut_gauche, num_ville, 'ville_haut_gauche')
+    bb = traitement_liste(b, direction_ville)
 
 
+    c = traitement_ville(ville_haut_droite2, num_ville2, 'ville_haut_droite2')
+    cc = traitement_liste(c, direction_ville2)
+
+    
+    d =traitement_ville(ville_haut_droite3, num_ville3, 'ville_haut_droite3')
+    dd = traitement_liste(d, direction_ville3)
+
+    
+    e = traitement_ville(ville_haut_droite4, num_ville4, 'ville_haut_droite4')
+    ee = traitement_liste_France(e, direction_ville4)
+    
+    
+    f =traitement_ville(bas_droite6, num_ville6, 'bas_droite6')
+    ff = traitement_liste(f, direction_ville6)
+
+    
+    g =traitement_ville(ville_bas_gauche9, num_ville9, 'ville_bas_gauche9')
+    gg = traitement_liste(g, direction_ville9)
+    
+    h =traitement_ville(ville_haut_droite5, num_ville5, 'ville_haut_droite5')
+    hh = traitement_liste(h, direction_ville5)
+    
+    i =traitement_ville(ville_bas_gauche8, num_ville8, 'ville_bas_gauche8')
+    ii = traitement_liste(i, direction_ville8)
+    
+    j =traitement_ville(ville_bas_droite7, num_ville7, 'ville_bas_droite7')
+    jj = traitement_liste(j, direction_ville7)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    liste.extend((aa,bb,cc,dd,ee,ff,gg,ii,jj))
+    print(liste)
+    return liste
 
 
 apport_pollu()
