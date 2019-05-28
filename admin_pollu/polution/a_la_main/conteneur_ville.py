baf56dc0ed602b7ba701cb29a88bab6d10cfc7f2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-28 18:59:42 +0200

    Add files via upload
    ouais mon code devient de plus en plus répétitif a un moment quand jfaisais les listcompréhension la jtai cool mais la...

---
+++ admin_pollu/polution/a_la_main/conteneur_ville.py
@@ -0,0 +1,65 @@
+#https://www.lachainemeteo.com/meteo-royaume-uni/ville-548409/previsions-meteo-london-colney-aujourdhui
+ville_haut_gauche = ['london', 'plymouth', 'cardiff', 'swansea', 'antwerp',
+                     'leeds', 'liverpool']#s, sse, se
+
+num_ville = [2, 40, 5746, 76, 9832, 6, 89]
+path1 = "https://www.lachainemeteo.com/meteo-royaume-uni/ville-{}/previsions-meteo-{}-aujourdhui"
+
+#https://www.lachainemeteo.com/meteo-belgique/golf-3490/previsions-meteo-duisburg-military-golf-club-aujourdhui
+ville_haut_droite = ['duisburg-military-golf-club','antwerp-golf-school', 'bruxelles',
+                     'liege', 'antwerp-golf-school']#S, SSO, SO, OSO
+
+num_ville1 = [3490, 9832, 6487,6557,9832]
+path2 = 'https://www.lachainemeteo.com/meteo-belgique/golf-{}/previsions-meteo-{}-aujourdhui'
+
+#https://www.lachainemeteo.com/meteo-pays-bas/ville-3732/previsions-meteo-amsterdam-aujourdhui
+ville_haut_droite2 = ['amsterdam', 'rotterdam']
+num_ville2 = [3732, 776]#SSO, SO, OSO
+path3 = 'https://www.lachainemeteo.com/meteo-pays-bas/ville-{}/previsions-meteo-{}-aujourdhui'
+
+#https://www.lachainemeteo.com/meteo-suisse/village-874436/previsions-meteo-aachen-aujourdhui
+ville_haut_droite3 = ['aachen','bern', 'zurich']
+num_ville3 = [874436, 44, 2097]#nno, no, ono, o, oso, so sso
+path4 = 'https://www.lachainemeteo.com/meteo-suisse/village-{}/previsions-meteo-{}-aujourdhui'
+
+#https://www.lachainemeteo.com/meteo-france/ville-312/previsions-meteo-lille-aujourdhui
+ville_haut_droite4 = ['lille', 'mulhouse', 'strasbourg' ]
+num_ville4 = [312, 18193, 309]#LILE / OSO SO SSO S SSE SE ESE
+                            #strasbourg + mulhouse: nno no ono o oso so sso
+path5 = 'https://www.lachainemeteo.com/meteo-france/ville-{}/previsions-meteo-{}-aujourdhui'
+
+
+bas_droite6 = ['nice']#nno no ono
+num_ville6 = [6590]
+path6 =''
+
+ville_bas_gauche9 =['perpignan']
+num_ville_9 = [6510]#ene, ne, nne, n nno no ono
+
+
+
+#https://www.lachainemeteo.com/meteo-allemagne/ville-33772/previsions-meteo-frankfurt-%28oder%29-aujourdhui
+ville_haut_droite5 = ['mainz', 'frankfurt', 'freiburg']
+num_ville5 = [2425,33772, 2598553]#oso,so,sso
+path8 = 'https://www.lachainemeteo.com/meteo-allemagne/ville-{}/previsions-meteo-{}-%28oder%29-aujourdhui'
+
+
+#https://www.lachainemeteo.com/meteo-espagne/village-1498961/previsions-meteo-cartagena-aujourdhui
+ville_bas_gauche8 = ['cartagena','barcelone','vigo',
+                    'san-sebastian',
+                    'bilbao','santander','gijon','oviedo','la-coruna']#nno, no , ono
+num_ville8 = [1498961,2558109,1519,1045, 2896, 356, 266, 5217,5217,4099]
+path9 = 'https://www.lachainemeteo.com/meteo-espagne/village-{}/previsions-meteo-{}-aujourdhui'
+
+#https://www.lachainemeteo.com/meteo-italie/ville-443727/previsions-meteo-baselga-del-bondone-aujourdhui
+ville_bas_droite7 = ['basel', 'torino']#nno, no, ono
+num_ville7 = [443727,2558109, ]
+path10 = 'https://www.lachainemeteo.com/meteo-italie/ville-{}/previsions-meteo-{}-aujourdhui'
+
+
+
+
+
+
+
+
---
+++ admin_pollu/polution/a_la_main/vent_voisin.py
@@ -0,0 +1,220 @@
+import os
+import cv2
+import json
+import requests
+import datetime
+import urllib.request
+from bs4 import *
+from colour import Color
+
+from boussole import *
+from conteneur_ville import *
+
+
+def vent_voisin(nombre, ville, situ):
+
+  
+    if situ == 'ville_haut_gauche':
+        path = "https://www.lachainemeteo.com/meteo-royaume-uni/ville-{}/previsions-meteo-{}-aujourdhui"
+        
+    elif situ == 'ville_haut_droite':
+        path = 'https://www.lachainemeteo.com/meteo-belgique/golf-{}/previsions-meteo-{}-aujourdhui'
+
+    elif situ == 'ville_haut_droite2':
+        path = 'https://www.lachainemeteo.com/meteo-pays-bas/ville-{}/previsions-meteo-{}-aujourdhui'
+
+    elif situ == 'ville_haut_droite3':
+        path = 'https://www.lachainemeteo.com/meteo-suisse/village-{}/previsions-meteo-{}-aujourdhui'
+
+    elif situ == 'ville_haut_droite4' or situ == 'ville_haut_droite6' or situ == 'ville_haut_droite9':
+        path = 'https://www.lachainemeteo.com/meteo-france/ville-{}/previsions-meteo-{}-aujourdhui'
+
+    elif situ == 'ville_haut_droite5':
+        path = 'https://www.lachainemeteo.com/meteo-allemagne/ville-{}/previsions-meteo-{}-%28oder%29-aujourdhui'
+
+    elif situ == 'ville_haut_droite8':
+        path = 'https://www.lachainemeteo.com/meteo-espagne/village-{}/previsions-meteo-{}-aujourdhui'
+        
+    elif situ == 'ville_haut_droite7':
+        path = 'https://www.lachainemeteo.com/meteo-italie/ville-{}/previsions-meteo-{}-aujourdhui'
+
+
+    path = path.format(nombre, ville)
+
+    r = requests.get(path)
+
+    page = r.content
+    soup = BeautifulSoup(page, "html.parser")
+
+    propriete = soup.find_all("div", {'class':'splitData'})
+
+    liste = str(propriete)
+
+    a = str(liste).find('scaleWindOrange"></i>')
+
+    code = liste[a:a+200]
+
+
+
+    c = str(code).find('Nord-Nord-Est')
+    
+    if c >= 0:
+        return 'Nord-Nord-Est'
+
+    d = str(code).find('Nord-Est')
+    
+    if d >= 0:
+        return 'Nord-Est'
+
+    e = str(code).find('Est-Nord-Est')
+    
+    if e >= 0:
+        return 'Est-Nord-Est'
+
+    g = str(code).find('Est-Sud-Est')
+    
+    if g >= 0:
+        return 'Est-Sud-Est'
+
+    h = str(code).find('Sud-Est')
+    
+    if h >= 0:
+        return 'Sud-Est'
+
+    i = str(code).find('Sud-Sud-Est')
+    
+    if i >= 0:
+        return 'Sud-Sud-Est'
+
+    k = str(code).find('Sud-Sud-Ouest')
+    if k >= 0:
+        return 'Sud-Sud-Ouest'
+
+    l = str(code).find('Sud-Ouest')
+    
+    if l >= 0:
+        return 'Sud-Ouest'
+
+    m = str(code).find('Ouest-Sud-Ouest')
+    
+    if m >= 0:
+        return 'Ouest-Sud-Ouest'
+
+    o = str(code).find('Ouest-Nord-Ouest')
+    
+    if o >= 0:
+        return 'Ouest-Nord-Ouest'
+
+    p = str(code).find('Nord-Ouest')
+    
+    if p >= 0:
+        return 'Nord-Ouest'
+
+    q = str(code).find('Nord-Nord-Ouest')
+    
+    if q >= 0:
+        return 'Nord-Nord-Ouest'
+
+
+    b = str(code).find('Nord')
+    
+    if b >= 0:
+        return 'Nord'
+
+    f = str(code).find('Est')
+    
+    if f >= 0:
+        return 'Est'
+
+    j = str(code).find('Sud')
+    
+    if j >= 0:
+        return 'Sud'
+
+    n = str(code).find('Ouest')
+    
+    if n >= 0:
+        return 'Ouest'
+
+ 
+    
+def traitement_ville(liste, liste1, nom_liste):
+
+
+    liste2 = []
+
+    c = 0
+    for i in liste:
+        print(i)
+        vent = vent_voisin(str(liste1[c]), i, nom_liste)
+        print(vent)
+        liste2.append((i, vent))
+        
+        c+=1
+    
+
+    
+def apport_pollu():
+    
+    a = traitement_ville(ville_haut_droite, num_ville1, 'ville_haut_droite')
+    b = traitement_ville(ville_haut_gauche, num_ville, 'ville_haut_gauche')
+    c = traitement_ville(ville_haut_droite2, num_ville2, 'ville_haut_droite2')
+    d =traitement_ville(ville_haut_droite3, num_ville3, 'ville_haut_droite3')
+    e =traitement_ville(ville_haut_droite4, num_ville4, 'ville_haut_droite4')
+    f =traitement_ville(bas_droite6, num_ville6, 'bas_droite6')
+    g =traitement_ville(ville_bas_gauche9, num_ville9, 'ville_bas_gauche9')
+    h =traitement_ville(ville_haut_droite5, num_ville5, 'ville_haut_droite5')
+    i =traitement_ville(ville_bas_gauche8, num_ville8, 'ville_bas_gauche8')
+    j =traitement_ville(ville_bas_droite7, num_ville7, 'ville_bas_droite7')
+    
+
+    print(a)
+
+apport_pollu()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
