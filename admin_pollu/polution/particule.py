80d8826316f6e627d225e9d3f7d12277b0668eba
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-30 16:56:19 +0200

    Update particule.py

--- admin_pollu/polution/particule.py
+++ admin_pollu/polution/particule.py
@@ -11,7 +11,8 @@ from PIL import Image, ImageDraw, ImageChops
 
 
 
-def particule(lieu, PARTICULE_PLAGE):
+
+def particule2(lieu):
 
     liste = []
     nb = []
@@ -44,81 +45,12 @@ def particule(lieu, PARTICULE_PLAGE):
 
     polution = nb
     #polution = int(polution)
-    
 
-    if polution <= 20:
-        PARTICULE_PLAGE['0_20'] += 1
-
-    elif polution >=21 and polution <= 40:
-        PARTICULE_PLAGE['21_40'] += 1
-        
-    elif polution >=41 and polution <= 60:
-        PARTICULE_PLAGE['41_60'] += 1
-        
-    elif polution >=61 and polution <= 80:
-        PARTICULE_PLAGE['61_80'] += 1
-        
-    elif polution >=81 and polution <= 100:
-        PARTICULE_PLAGE['81_100'] += 1
-        
-    elif polution >=101 and polution <= 120:
-        PARTICULE_PLAGE['101_120'] += 1
-        
-    elif polution >=121 and polution <= 140:
-        PARTICULE_PLAGE['121_140'] += 1
-        
-    elif polution >=141 and polution <= 160:
-        PARTICULE_PLAGE['141_160'] += 1
-        
-    elif polution >=161 and polution <= 180:
-        PARTICULE_PLAGE['161_180'] += 1
-        
-    elif polution >=181 and polution <= 200:
-        PARTICULE_PLAGE['181_200'] += 1
-        
-    elif polution >200:
-        PARTICULE_PLAGE['>200'] += 1
-
-
-def particule2(lieu, PARTICULE):
-
-    liste = []
-    nb = []
-
-    path = "https://air.plumelabs.com/fr/live/{}".format(lieu)
-
-    r = requests.get(path)
-
-
-    page = r.content
-    soup = BeautifulSoup(page, "html.parser")
-
-    propriete = soup.find_all("div", {'class':'report__pi-number'})
-    for i in propriete:
-        liste.append(i.get_text())
-    for i in liste:
-        for j in i:
-            try:
-                j = int(j)
-                if j == int(j):
-                    nb.append(str(j))
-            except:
-                pass
-
-    
-    nb = ''.join(nb)
-    nb = int(nb)
-
-    phrase_clé = "a atteint un niveau élevé de pollution. Supérieur à la limite maximum pour 24h établie par l'OMS"
-
-    polution = nb
-    #polution = int(polution)
-    PARTICULE['nb'] += nb
     return nb
 
 
     
-def france(lieu, VILLE_POLLUE2018):
+def france(lieu):
 
 
     liste = ["lyon", "marseille","paris","roubaix"]
@@ -128,22 +60,22 @@ def france(lieu, VILLE_POLLUE2018):
     for i in liste:
 
         if lieu == liste[0]:
-            VILLE_POLLUE2018['un'] += 1
+            return 'un'
             break
 
         elif lieu == liste[1]:
-            VILLE_POLLUE2018['deux'] += 1
+            return 'deux'
             break
 
         elif lieu == liste[2]:
-            VILLE_POLLUE2018['trois'] += 1
+            return 'trois'
             break
 
         elif lieu == liste[3]:
-            VILLE_POLLUE2018['quattre'] += 1
+            return 'quattre'
             break
         else:
-            VILLE_POLLUE2018['non'] += 1
+            return 'non'
             break
             
 
@@ -152,62 +84,69 @@ def france(lieu, VILLE_POLLUE2018):
     
 
 
-def industrie(lieu, REGION_INDUSTRIEL_POLLUEE):
+def industrie(lieu):
 
 
-
-    path = "https://fr.wikipedia.org/wiki/{}".format(lieu) 
-
-    r = requests.get(path)
-
-    page = r.content
-    soup = BeautifulSoup(page, "html.parser")
-    propriete = soup.find('table',attrs={"class":u"infobox_v2"})
-    propriete = str(propriete)
-
-    try:
-        code_postal = propriete[5649:5654]
-        code_postal = int(code_postal)
-        
-    except:
-        pass
-
-    liste = []
-
-    path = "http://www.cartesfrance.fr/recherche/?q={}".format(code_postal)
-
-    r = requests.get(path)
-
-    page = r.content
-    soup = BeautifulSoup(page, "html.parser")
+##
+##    path = "https://fr.wikipedia.org/wiki/{}".format(lieu) 
+##
+##    r = requests.get(path)
+##
+##    page = r.content
+##    soup = BeautifulSoup(page, "html.parser")
+##    propriete = soup.find('table',attrs={"class":u"infobox_v2"})
+##    propriete = str(propriete)
+##
+##    try:
+##        code_postal = propriete[5649:5654]
+##        code_postal = int(code_postal)
+##        
+##    except:
+##        pass
+##
+##    liste = []
+##
+##    path = "http://www.cartesfrance.fr/recherche/?q={}".format(code_postal)
+##
+##    r = requests.get(path)
+##
+##    page = r.content
+##    soup = BeautifulSoup(page, "html.parser")
+##    
+##    propriete = soup.find_all('Département')
+##
+##
+##
+##
+##    pole_poluant = {'1':'Nord',
+##                    '2':'Bouches-du-Rhône',
+##                    '3':'Moselle',
+##                    '4':'Seine-Maritime',
+##                    '5':'Loire-Atlantique',
+##                    '6':'Haute-Normandie',
+##                    '7':'Meurthe-et-Moselle',
+##                    '8':'Seine-Maritime',
+##                    '9':'Rhône'
+##
+##                    }
+    if lieu == 'lyon':
+        return 'oui'
+    elif lieu == 'paris':
+        return 'non'
+    elif lieu == 'marseille':
+        return 'oui'
     
-    propriete = soup.find_all('Département')
+##    for i in pole_poluant.keys():
+##        a = str(soup).find(str(pole_poluant[i]))
+##        print(a,pole_poluant[i])
 
 
-
-
-    pole_poluant = {'1':'Nord',
-                    '2':'Bouches-du-Rhône',
-                    '3':'Moselle',
-                    '4':'Seine-Maritime',
-                    '5':'Loire-Atlantique',
-                    '6':'Haute-Normandie',
-                    '7':'Meurthe-et-Moselle',
-                    '8':'Seine-Maritime',
-                    '9':'Rhône'
-
-                    }
-
-
-    for i in pole_poluant.keys():
-        a = str(soup).find(str(pole_poluant[i]))
-    #    print(a,pole_poluant[i])
-
-    if a > 0:
-        REGION_INDUSTRIEL_POLLUEE['oui'] += 1
-
-    else:
-        REGION_INDUSTRIEL_POLLUEE['non'] += 1
+##
+##    if a > 0:
+##        return 'oui'
+##
+##    else:
+##        return 'non'
         
 
 
