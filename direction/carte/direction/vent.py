f214b7efda5af0183ba4d1dc6bdf11ef8d826408
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-31 19:51:06 +0200

    Update vent.py

--- direction/carte/direction/vent.py
+++ direction/carte/direction/vent.py
@@ -5,7 +5,7 @@ from math import *
 
 
 def le_vent(lieu):
-    print(lieu)
+
     clé = '5a72ceae1feda40543d5844b2e04a205'
     
     localisation = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}".format(lieu,clé)
