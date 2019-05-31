f8f721f607afb6aeb91d56bbd08d07fb5a386b8b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-31 16:33:14 +0200

    Update addresse.py
    geopy geocoder nominapy pygeocode jai tout test

--- direction/carte/direction/addresse.py
+++ direction/carte/direction/addresse.py
@@ -1,59 +1,20 @@
-from geopy.geocoders import Nominatim
-import requests
-from bs4 import *
-from math import *
 
-
-def par_lat_par_long(lat, long):
-
-    path = 'https://nominatim.openstreetmap.org/reverse?format=xml&lat={}&lon={}&zoom=18&addressdetails=1'
-    path = path.format(lat, long)
-    
-    r = requests.get(path)
-
-    page = r.content
-    soup = BeautifulSoup(page, "html.parser")
-
-    liste = []
-    liste.append(soup.text)
-
-    liste = liste[0][2:]
-    
-    liste1 = []
-    mot = ''
-
-    c = 0
-    for i in liste:
-    
-        if i == ' ':
-            liste1.append(mot)
-            mot = ''
-        else:
-            mot += i
-            
-        c+=1
-
-
-
-    c = 0
-    liste2 = []
-    code = ''
-    
-    for i in liste1:
-
-        try:
-            o = int(i[:-1])
-            if o == int(o) and c >= 6:
-                break
-            i = str(i)
-        except:
-            pass
-
-        
-        liste2.append(i)
-        
-        c+=1
-
-
-    #print(' '.join(liste2))
-    return ' '.join(liste2)
+# Python3 program for reverse geocoding. 
+  
+# importing necessary libraries 
+import reverse_geocoder as rg 
+import pprint 
+  
+def reverseGeocode(coordinates): 
+    result = rg.search(coordinates) 
+      
+    # result is a list containing ordered dictionary. 
+    pprint.pprint(result)  
+  
+# Driver function 
+if __name__=="__main__": 
+      
+    # Coorinates tuple.Can contain more than one pair. 
+    coordinates =(44.7333, 5.0333) 
+      
+    reverseGeocode(coordinates) 
