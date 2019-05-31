bf48f3a88cd4ab0175f96ec25c82585a1ec91dbc
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-31 19:50:54 +0200

    Update superficie.py

--- direction/carte/direction/superficie.py
+++ direction/carte/direction/superficie.py
@@ -3,11 +3,11 @@ from bs4 import *
 from math import *
 
 
-def superficie(ville):
+def superficie_ville(ville):
 
     path = 'https://www.google.com/search?ei=wjnsXOzeIZCAjLsPm62CgAo&q={}+superficie&oq={}+superficie&gs_l=psy-ab.3..33i160l3.47870.49141..49264...0.0..0.316.1867.0j9j1j1......0....1..gws-wiz.......0i71j35i39j0i20i263j0i67j0j0i10j0i203j0i22i30.KKnu850MPRU'
     path = path.format(ville, ville)
-
+ 
     r = requests.get(path)
     page = r.content
     
@@ -26,7 +26,7 @@ def superficie(ville):
         number = ''
         for j in i:
             if j == ',':
-                number+= str(j)
+                number+= str('.')
             try:
                 j = int(j)
                 if j == int(j):
@@ -39,12 +39,11 @@ def superficie(ville):
         if kilometre_carre == True:
             break
 
-    print(number)
-
+    return number
 
 
         
-superficie('paris')
+#superficie_ville('Jardin')
 
 
 
