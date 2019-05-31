5bc70cddacb03bbf06ea666280ba6a5686960059
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-31 19:50:17 +0200

    Update addresse.py

--- direction/carte/direction/addresse.py
+++ direction/carte/direction/addresse.py
@@ -3,11 +3,12 @@ import pprint
   
 def reverseGeocode(coordinates): 
     result = rg.search(coordinates) 
-    
+ 
     return result
+
   
 
-      
+
 def dress_to_ville(lat, long):
     
     coordinates =(lat, long) 
@@ -17,11 +18,16 @@ def dress_to_ville(lat, long):
     for i,j in a[0].items():
         if c == 2:
             liste.append(j)
-
+        elif c == 3:
+            liste.append(j)
         c+=1
 
-    #print(liste)
-    return liste[0]
+    ad = str(liste[0]+'+'+liste[1])
+    return liste[0], ad
 
-    
+##if __name__ == '__main__':
+##
+##    a = dress_to_ville(45.4963988, 4.9078121)
+##    print(a[0])
+##    print(a[1])
     
