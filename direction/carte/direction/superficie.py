0faa50433cc98b1ebf50235a46f87bc095f60ae7
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-02 12:18:22 +0200

    Update superficie.py
    oups mais le vent va au sso de PARTOUT sauf reims et j'ai fais toutes les villes que je connaissais c 'est trop zarb

--- direction/carte/direction/superficie.py
+++ direction/carte/direction/superficie.py
@@ -4,7 +4,7 @@ from math import *
 
 
 def superficie_ville(ville):
-
+    #print(ville,'VILLLLLLLLLLLLLLLLLLLLLLE')
     path = 'https://www.google.com/search?ei=wjnsXOzeIZCAjLsPm62CgAo&q={}+superficie&oq={}+superficie&gs_l=psy-ab.3..33i160l3.47870.49141..49264...0.0..0.316.1867.0j9j1j1......0....1..gws-wiz.......0i71j35i39j0i20i263j0i67j0j0i10j0i203j0i22i30.KKnu850MPRU'
     path = path.format(ville, ville)
  
@@ -22,28 +22,35 @@ def superficie_ville(ville):
 
 
     kilometre_carre = ''
+    
     for i in liste:
         number = ''
+        numbe = ''
+        if kilometre_carre == True:
+            break
         for j in i:
-            if j == ',':
+            if j == ',' and numbe == True:
                 number+= str('.')
             try:
                 j = int(j)
                 if j == int(j):
                     number+=str(j)
+                    numbe = True
             except:
                 pass
             if j == '²':
                 kilometre_carre = True
+                break
                 
         if kilometre_carre == True:
             break
-
+        
+    #print(number)
     return number
 
 
         
-#superficie_ville('Jardin')
+#superficie_ville("marseille")
 
 
 
