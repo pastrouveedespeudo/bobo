f92e2168fe2ed20ed94dbf1139cd0f697bace084
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-02 16:23:36 +0200

    Update all.py

--- admin_pollu/polution/a_la_main/all.py
+++ admin_pollu/polution/a_la_main/all.py
@@ -1,55 +1,76 @@
 import psycopg2
 
+from analyse import *
 
 
-
-conn = psycopg2.connect(database='datu8fkornnndh',
-                         user='pwtfmpvfpsujtw',
-                         host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                         password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696')
-
-
-cursor = conn.cursor()
-
-liste1 = ['angrais', 'incendie', 'pression',
-          'vent', 'météo', 'climat', 'saison',
-          'region_industrielle',
-          'population_active', 'traffique',
-          'heure', 'weekend', 'bouchon',
-          'activité', 'diesel', 'eruption',
-          'jour_nuit', 'polenne', 'voisin',
-          'pos_france']
-
-liste2 = ['conditions']
-
-
-for i in liste1:
+def donnée():
     
-    cursor.execute("""select *  from {};""".format(i))
+    conn = psycopg2.connect(database='datu8fkornnndh',
+                             user='pwtfmpvfpsujtw',
+                             host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
+                             password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696')
 
 
-    conn.commit()
+    cursor = conn.cursor()
+
+    liste1 = ['angrais', 'incendie', 'pression',
+              'vent', 'météo', 'climat', 'saison',
+              'region_industrielle',
+              'population_active', 'traffique',
+              'heure', 'weekend', 'bouchon',
+              'activité', 'diesel', 'eruption',
+              'jour_nuit', 'polenne', 'voisin',
+              'pos_france']
+
+    liste2 = ['conditions']
+
+
+##    for condi in liste1:
+##        
+##        cursor.execute("""select *  from {};""".format(condi))
+##
+##
+##        conn.commit()
+##
+##
+##
+##      
+##        rows = cursor.fetchall()
+##        liste = [i for i in rows]
+##
+##
+##        for i in liste:
+##            print(condi)
+##            print(i[-1], i[-2])
+##
+##        print('\n\n')
 
 
 
-  
-    rows = cursor.fetchall()
-    liste = [i for i in rows]
+    liste_ville = ['lyon']
+    #liste_ville = ['lyon', 'paris', 'marseille']
+
+    for i in liste_ville:
+
+        cursor.execute("""select *  from conditions2 where nom_ville = '{}';""".format(i))
 
 
-    for i in liste:
-        print(i)
+        conn.commit()
+
+        rows = cursor.fetchall()
+        liste = [i for i in rows]
+        minimum = condition_min(liste)
+        liste1, liste2, liste3, liste4 = différence(liste, minimum)
+        conclusion(liste1, liste2, liste3, liste4)
+            
+            
+            
+        print('\n')
+        print('\n')
+        print('\n')
 
 
-
-
-
-
-
-
-
-
-
+donnée()
 
 
 
