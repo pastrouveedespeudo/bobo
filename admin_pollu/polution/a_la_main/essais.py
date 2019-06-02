9950229deada0d6e35ce0a7cc610e2ce4e891740
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-02 23:32:21 +0200

    Add files via upload
    le but c de comparer chaque condition et qua la fin ca sorte tous les weekend, toutes les heures de pointe ect et d'en faire la moyenne poru a la fin avoir un score

--- admin_pollu/polution/a_la_main/essais.py
+++ admin_pollu/polution/a_la_main/essais.py
@@ -1,21 +1,132 @@
 
+
 import psycopg2
 
 
 
+
+def yo():
+    conn = psycopg2.connect(database='datu8fkornnndh',
+                             user='pwtfmpvfpsujtw',
+                             host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
+                             password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696')
+    cursor = conn.cursor()
+    aaa = 'lyon'
+
+    cursor.execute("""select *  from conditions2 where nom_ville = '{}' order by nombre_particule ;""".format(aaa))
+    
+    rows = cursor.fetchall()
+    liste = [i for i in rows]
+
+    return liste[0]
+
+
+
+
+
 conn = psycopg2.connect(database='datu8fkornnndh',
-                        user='pwtfmpvfpsujtw',
-                        host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                        password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696')  
-
+                         user='pwtfmpvfpsujtw',
+                         host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
+                         password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696')
 cursor = conn.cursor()
+aaa = 'lyon'
 
-cursor.execute("""select * from ville;""")
+cursor.execute("""select *  from conditions2 where nom_ville = '{}';""".format(aaa))
 
 
 conn.commit()
+
 rows = cursor.fetchall()
 liste = [i for i in rows]
+en_cours = []
+
+a = yo()
+
+a = list(a[2:-2])
+#print(a)
+#print('\n')
+deja = []
+debut = ''
+
+
+
+liste1 = []
+diffdiff = []
 
 for i in liste:
-    print(i)
+    c = 0
+    compteur = 0
+    i = list(i[2:-2])
+    diff = []
+    for j in i:
+        if j != a[c]:
+            compteur +=1
+            diff.append(j)
+
+        c+=1
+    diffdiff.append(diff)
+    liste1.append(compteur)
+  
+liste123 = []
+
+difffff = [[],[],[],[],[],[],[],
+           [],[],[],[],[],[],[],
+           [],[],[],[],[],[],[],
+           [],[],[],[],[],[],[],[],
+           [],[],[],[],[],[],
+           [],[],[],[],[],[],[],
+           [],[],[],[],
+           [],[],[],[],[],[],
+           [],[],[],[],[],[]]
+
+
+for i in diffdiff:
+    difffff[len(i)].append(i)
+
+
+c = 0
+for i in difffff:
+    
+    if i == []:
+        pass
+    else:
+        print(c)
+        
+        print(i)
+        print('\n')
+
+
+    c+=1
+    
+   
+
+
+
+   
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
