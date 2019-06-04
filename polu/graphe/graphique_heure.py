9e56851afcdcb9d49a35eee6a6d4c27c167e5cdb
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-04 22:28:50 +0200

    Update graphique_heure.py
    t'u vas pouvoir en refaire t'as climat a rajouter et incendie ect

--- polu/graphe/graphique_heure.py
+++ polu/graphe/graphique_heure.py
@@ -14,14 +14,14 @@ from .fonction_graphe import new
 
 def visu_horraire(ville):
     
-    conn = psycopg2.connect(database='bobo',
-                            user='postgres',
-                            host='127.0.0.1',
-                            password='tiotiotio333')  
+    conn = psycopg2.connect(database='datu8fkornnndh',
+                             user='pwtfmpvfpsujtw',
+                             host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
+                             password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
 
     cursor = conn.cursor()
     
-    sql = ("""SELECT HEURE, particule FROM ville
+    sql = ("""SELECT HEURE, nombre_particule FROM heure
             WHERE nom_ville = %s;""")
     
     values = (ville)
@@ -105,7 +105,7 @@ def diagramme_heure(pointe, non_pointe,
     plt.savefig(nouveau)
     plt.close()
     
-    shutil.move(nouveau, r'C:\Users\jeanbaptiste\bobo\bobo\static\popo')
+    shutil.move(nouveau, '/app/static/popo')
 
     return nouveau
 
