e8c7ef8e84ece7bb89155cd873fb70da37ba1d0b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-04 22:31:22 +0200

    Update graphique_manif.py
    cool prend ton temps la derniere fois ca m'avais assomé

--- polu/graphe/graphique_manif.py
+++ polu/graphe/graphique_manif.py
@@ -11,14 +11,14 @@ from .fonction_graphe import new
 
 def visu_manif(ville):
     
-    conn = psycopg2.connect(database='bobo',
-                            user='postgres',
-                            host='127.0.0.1',
-                            password='tiotiotio333')  
+    conn = psycopg2.connect(database='datu8fkornnndh',
+                             user='pwtfmpvfpsujtw',
+                             host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
+                             password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696')  
 
     cursor = conn.cursor()
     
-    sql = ("""SELECT ACTIVITE_EXEPTIONNELLE, particule FROM ville
+    sql = ("""SELECT ACTIVITE_EXEPTIONNELLE, nombre_particule FROM activité
             WHERE nom_ville = %s;""")
     
     values = (ville)
@@ -81,7 +81,7 @@ def diagramme_manif(donnée_manif, donnée_non_manif,
     
     plt.savefig(nouveau)
     plt.clf()
-    shutil.move(nouveau, r'C:\Users\jeanbaptiste\bobo\bobo\static\popo')
+    shutil.move(nouveau, '/app/static/popo')
     return nouveau
 
 
