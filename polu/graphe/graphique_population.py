39f02e027abdb46d4d0850fcd639920025534d15
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-04 22:36:37 +0200

    Update graphique_population.py

--- polu/graphe/graphique_population.py
+++ polu/graphe/graphique_population.py
@@ -10,14 +10,16 @@ from .fonction_graphe import new
 
 def visu_population():
     
-    conn = psycopg2.connect(database='bobo',
-                            user='postgres',
-                            host='127.0.0.1',
-                            password='tiotiotio333')  
+    conn = psycopg2.connect(database='datu8fkornnndh',
+                             user='pwtfmpvfpsujtw',
+                             host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
+                             password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696') 
 
     cursor = conn.cursor()
     
-    cursor.execute("""SELECT POPULATION_ACTIVE_HABITANT, nom_ville, particule FROM ville;""")
+    cursor.execute("""SELECT POPULATION_ACTIVE_HABITANT,
+                    nom_ville,
+                    nombre_particule FROM conditions2;""")
     
     rows = cursor.fetchall()
     liste = [i for i in rows]
@@ -80,6 +82,6 @@ def diagramme_population(donnée_lyon, donnée_paris, donnée_marseille,
     
     plt.savefig(nouveau)
     plt.clf()
-    shutil.move(nouveau, r'C:\Users\jeanbaptiste\bobo\bobo\static\popo')
+    shutil.move(nouveau, '/app/static/popo')
 
     return nouveau
