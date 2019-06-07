32081e72a41c051ade5831ab3e6bd351c3e43e2c
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-08 01:14:36 +0200

    Update essais.py
    si t'apprends mal aussi c la faute a personne ptetre ca marchera ptetre pas

--- photoo/magasins/essais.py
+++ photoo/magasins/essais.py
@@ -1,9 +1,9 @@
-import requests
 from bs4 import BeautifulSoup
-
-url = "https://www.google.com/search?ei=Gnn5XJP5KIu5gweW1qaQBQ&q=Coiffure%20Marilyne+crest+horraires&oq=Coiffure%20Marilyne+crest+horraires"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-tb = soup.find_all('div')
-
-print(tb)
+import requests
+metacriticaddress = "http://www.metacritic.com/search/tv/game%20of%20thrones/results"
+r = requests.get(metacriticaddress, headers={
+     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"
+ })
+soup = BeautifulSoup(r.content, "html.parser")
+first_result = soup.find_all("li", class_="first_result")
+print(first_result)
