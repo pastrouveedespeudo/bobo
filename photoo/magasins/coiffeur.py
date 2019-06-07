b707b8e315afcce97bb3cde7752b503c1fdf5697
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-07 13:43:23 +0200

    Update coiffeur.py
    pose popo puis pose chat

--- photoo/magasins/coiffeur.py
+++ photoo/magasins/coiffeur.py
@@ -8,10 +8,48 @@ LISTE = ['coiffure', 'COIFFURE', 'salon', 'salon de coiffure']
 
 
 def rayon(ville):
-    pass
+    path = "https://www.villorama.com/ville/{}/villes-proches.html"
+    
+    path = path.format(ville)
+    r = requests.get(path)
+    page = r.content
+    
+    soup = BeautifulSoup(page, "html.parser")
+    
+    propriete = soup.find_all("a")
 
+    liste = []
 
+    for i in propriete:
+        if i.string == None:
+            pass
+        elif i.string == 'France' or i.string == 'Distance à vol d\'oiseau'\
+             or i.string == 'Communes proches' or i.string == 'Présentation'\
+             or i.string == 'En bref' or i.string == 'Sport'\
+             or i.string == 'Météo' or i.string == 'Prévisions météo'\
+             or i.string == 'Sorties culturelles' or i.string == 'Agenda'\
+             or i.string == 'Hôtels' or i.string == 'Midi Moins Une'\
+             or i.string == 'mentions légales' or i.string == 'Recrutement'\
+             or i.string == 'Midi Moins Une' or i.string == 'Gestion des cookies'\
+             or i.string == 'Tourisme' or i.string == 'Office de Tourisme'\
+             or i.string == 'Distance routière' or i.string == 'Mairie'\
+             or i.string == 'Plan' or i.string == 'Itinéraire' or i.string == 'Annuaire Services'\
+             or i.string == 'Résultats d\'élections' or i.string == 'Loisirs' or i.string == 'Spectacles'\
+             or i.string == 'Concerts' or i.string == 'Théâtre' or i.string == 'Historique météo'\
+             or i.string == 'Rencontres' or i.string == 'Autres informations' or i.string == 'Cinéma'\
+             or i.string == 'Sites web' or i.string == 'Identifiez-vous' or i.string == 'Votre espace'\
+             or i.string == 'Publicité' or i.string == '' or i.string == 'Conditions d\'Utilisation'\
+             or i.string == '' or i.string == 'Etablir un lien':
+             
+             
+            pass
+             
+        else:
+            liste.append(i.string)
 
+    liste = liste[3:]
+    print(liste)
+    return liste
 
 
 def ville(ville):
@@ -26,9 +64,16 @@ def ville(ville):
     
     propriete = soup.find_all("span")
 
-    for i in propriete:
-        print(i)
+    liste = []
 
+    for i in propriete:
+        #print(i.string)
+        for j in LISTE:
+            a = str(i.string).find(str(j))
+            if a >= 0:
+                liste.append(i.string)
+    print(liste)
+    return liste
 
 def horraire(nom, ville):
 
@@ -48,10 +93,10 @@ def horraire(nom, ville):
 
     
 
-#ville('crest')
-horraire("maryline coiffure crest", "crest")
+ville('crest')
+#horraire("maryline coiffure crest", "crest")
 
-
+#rayon('crest')
 
 
 
