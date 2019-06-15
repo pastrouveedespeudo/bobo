f29f6a0a169db9750938c8491c256d5d197630e6
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-15 14:23:46 +0200

    Add files via upload

---
+++ static/js/prediction.js
@@ -0,0 +1,140 @@
+
+//Here we call database and analysa2.py for lyon
+
+jQuery("#lyon").on("click", function(e){
+
+    //we "raise" the pred div, display you choose lyon city
+    //and display the loading logo
+    document.getElementById('pred').innerHTML = ""
+    document.getElementById('choix').innerHTML = 'Vous avez choisis Lyon';
+    document.getElementById('chargement').style.display = "block";
+
+    jQuery.ajax({
+        data:{
+            'lyon':'lyon',
+        },
+        type:"POST",
+        url:"/polution/prediction"
+    })
+    .done(function(data){
+        if (data.error){
+            jQuery("#monCadreAlert").text(data.error);
+            jQuery("#is_save"); 
+        }
+        else{
+            jQuery("#is_save").html(data.data);
+            jQuery("#monCadreAlert");
+
+            document.getElementById('chargement').style.display = "none";      
+            
+            if (data == "pas de donnée"){
+                document.getElementById('pred').innerHTML = "<br><br><strong>pas de donnée</strong>";
+            }
+            else{
+              
+              document.getElementById('pred').innerHTML = '<br><br>' +
+                                                          'Nous pensons que le taux de polution est de: '+
+                                                          '<strong>' +
+                                                          data +
+                                                          ' AQI</strong>';
+            };      
+        };
+        
+    });
+});
+
+
+
+
+//Here we call database and analysa2.py for paris
+jQuery("#paris").on("click", function(e){
+
+
+    //we "raise" the pred div, display you choose paris city
+    //and display the loading logo
+    document.getElementById('choix').innerHTML = 'Vous avez choisis Paris';
+    document.getElementById('chargement').style.display = "block";
+    document.getElementById('pred').innerHTML = ""
+    
+    jQuery.ajax({
+        data:{
+            'paris':'paris',
+        },
+        type:"POST",
+        url:"/polution/prediction"
+    })
+    .done(function(data){
+        if (data.error){
+            jQuery("#monCadreAlert").text(data.error);
+            jQuery("#is_save"); 
+        }
+        else{
+            jQuery("#is_save").html(data.data);
+            jQuery("#monCadreAlert");
+
+            document.getElementById('chargement').style.display = "none";
+            
+          if (data == "pas de donnée"){
+                document.getElementById('pred').innerHTML = "<br><br><strong>pas de donnée</strong>";
+            }
+            else{
+            document.getElementById('pred').innerHTML = '<br><br>' +
+                                                        'Nous pensons que le taux de polution est de : '+
+                                                        '<strong>' +
+                                                        data +
+                                                        '</strong>';
+
+            };            
+        };
+    });
+});
+
+
+
+
+//Here we call database and analysa2.py for marseille
+jQuery("#marseille").on("click", function(e){
+
+
+    //we "raise" the pred div, display you choose marseille city
+    //and display the loading logo
+    document.getElementById('choix').innerHTML = 'Vous avez choisis Marseille';
+    document.getElementById('chargement').style.display = "block";
+    document.getElementById('pred').innerHTML = ""
+    
+    jQuery.ajax({
+        data:{
+            'marseille':'marseille',
+        },
+        type:"POST",
+        url:"/polution/prediction"
+    })
+    .done(function(data){
+        if (data.error){
+            jQuery("#monCadreAlert").text(data.error);
+            jQuery("#is_save"); 
+        }
+        else{
+            jQuery("#is_save").html(data.data);
+            jQuery("#monCadreAlert");
+            
+            document.getElementById('chargement').style.display = "none";
+            
+          if (data == "pas de donnée"){
+                document.getElementById('pred').innerHTML = "<br><br><strong>pas de donnée</strong>";
+            }
+
+            else{
+              document.getElementById('pred').innerHTML = '<br><br>' +
+                                                        'Nous pensons que le taux de polution est de : '+
+                                                        '<strong>' +
+                                                        data +
+                                                        '</strong>';
+            };            
+        }; 
+    });
+});
+
+
+
+
