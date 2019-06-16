9aee85c8c49036c3dfaf30f6acf28a6954290552
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-16 15:37:34 +0200

    Update eruption.py
    ca sert a rien le comment du dessus mais c trop classe ^^

--- polu/donnée_site/eruption.py
+++ polu/donnée_site/eruption.py
@@ -1,3 +1,5 @@
+"""BS4, Requests, DateTime, Str.Find 16/04/2019"""
+
 import os
 import cv2
 import json
@@ -9,81 +11,69 @@ from bs4 import *
 
 
 def eruption():
-
+    """Here we get eruption during the last week"""
         
     date = datetime.datetime.now()
-    
-    
-    mois = date.month
-    année = date.year
 
-    
-    mois = str(mois)
-    année = str(année)
+    month = date.month
+    year = date.year
 
+    month = str(month)   #-> We get time of the current day. 
+    year = str(year)  #and tranform it to str stuff.
+                        
 
-
+                        #because we'ill compare it to this dico.
     dico = {'1':'January', '2':'February', '3':'March','4':'April','5':'May','6':'June',
             '7':'July','8':'August','9':'September','10':'October','11':'November','12':'December'}
 
-    le_mois = ''
-
-    for cle, valeur in dico.items():
-        if str(mois) == cle:
-            le_mois = valeur
+    this_month = ''
+    for key, value in dico.items():#We trying to search the current month and attribute it 
+                                    #to variable this_month,
+        if str(month) == key:        #By a for loop. Thank to that we can
+                                    #search eruption from this week from this month.
+            this_month = value
 
 
     path = "https://www.volcanodiscovery.com/fr/volcanoes/today.html"
-
     r = requests.get(path)
-
     page = r.content
-
     soup = BeautifulSoup(page, "html.parser")
-
-    
-    propriete = soup.find_all("div", {"class":"ln"})
-
+    Property = soup.find_all("div", {"class":"ln"}) #we are looking for this tag
+                                                     #and attribute container of it to a list 
     liste = []
+    liste.append(str(Property))
 
-    liste.append(str(propriete))
-
-
-    #print(liste)
 
     liste2 = []
+    for i in range(7):      #7 times for a week we searching the last seven days on this site web and return
+                            #yes for eruption if list isn't empty.
 
-    for i in range(7):
-
-        jour = date.day
-        jour = str(jour-i)
-
-        a_chercher = jour +' '+le_mois + ' ' + année
-        a_chercher1 = le_mois + ' ' + jour + ' ' + année
-
+        day = date.day
+        day = str(day-i) #for example: today : 8/06 for 7times we doing: 8-n-1.
         
-        a_chercher2 = jour+'-'+mois+'-'+année
-        a_chercher3 = jour+'-'+le_mois+'-'+année
-        a_chercher4 = le_mois + ' ' + jour + ', ' + année
+        #Here are the date schema possibilities of this site.
+        to_search = day +' '+ this_month + ' ' + year        
+        to_search1 = this_month + ' ' + day + ' ' + year
+        to_search2 = day + '-' + month + '-' +year
+        to_search3 = day + '-' + this_month + '-' + year
+        to_search4 = this_month + ' ' + day + ', ' + year
 
+        a = str(liste).find(str(to_search))    #if this schem is present it return >= 0.
+        b = str(liste).find(str(to_search1))
+        c = str(liste).find(str(to_search2))
+        d = str(liste).find(str(to_search3))
+        e = str(liste).find(str(to_search4))
 
-        a = str(liste).find(str(a_chercher))
-        b = str(liste).find(str(a_chercher1))
-        c = str(liste).find(str(a_chercher2))
-        d = str(liste).find(str(a_chercher3))
-        e = str(liste).find(str(a_chercher4))
-
-        if a >= 0 or b >= 0 or c >=0 or d >= 0 or e >=0:
-            jour = str(jour) + ' ' + mois
-            liste2.append(jour)
+        if a >= 0 or b >= 0 or c >=0 or d >= 0 or e >=0: #If >= 0 We append it to a list.
+            day = str(day) + ' ' + month
+            liste2.append(day)
 
 
     if liste2 != []:
-        return 'oui'
+        return 'oui'#yes for erupt.
     
 
     
-
 
 
 
