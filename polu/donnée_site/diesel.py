077accd9583c90f63bc22666ec499e25f4a7c63b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-16 15:22:15 +0200

    Update diesel.py

--- polu/donnée_site/diesel.py
+++ polu/donnée_site/diesel.py
@@ -1,3 +1,6 @@
+"""BS4, Request, str.find 16/04/2019"""
+
+
 import os
 import cv2
 import json
@@ -7,37 +10,43 @@ import urllib.request
 from bs4 import *
 
 
-def recup_balise():
+def recup_tag():
+    """Here we get diesel and dollars courses
+    to analyze in France the sale of siesel and
+    therefore the rate of diesel car right now"""
 
-    dol = cours_dollar()
+
+
+    dol = course_dollars()  #we call course_dollars()
+                            #because dollars // price diesel
     
     
-    path = "https://prixdubaril.com/"
-
-    r = requests.get(path)
-
+    path = "https://prixdubaril.com/" #We take url site
+    r = requests.get(path)#requests stuff
     page = r.content
-    soup = BeautifulSoup(page, "html.parser")
+    soup = BeautifulSoup(page, "html.parser")   #Soup stuff
 
     propriete = soup.find_all("div", {'class':'carburant_red'})
-
+                                    #We searching this balise
+    
     a = str(propriete).find('Gas')
     b = str(propriete).find('Gas+')
     c = str(propriete).find('Gazole')
-    d = str(propriete).find('Gazole+')
+    d = str(propriete).find('Gazole+')#And search this str.
+                                      #if variable are found into propriete (balise)
+                                      #so it return number >= 0.
     
 
     gas = False
-    gasplus = False
+    gasplus = False #Initializing gas and gasplus (diesel) to False
     
-    if a or c >= 0:
+    if a or c >= 0: #If the returning number is >= 0 so it become True
         gas = True
     elif b or d >=0:
         gasplus = True
 
 
-    
-
+    #Now we playing with condition and return it for "our analysis"
     if gas == True and gasplus == True and dol  == 'dollard baisse  ':
         return 'tres fort'
 
@@ -60,7 +69,8 @@ def recup_balise():
 
 
     
-def cours_dollar():
+def course_dollars():
+    """Here we try to get course of dollars BeautifulSoup and Request"""
     
     path = "https://prixdubaril.com/"
 
@@ -82,14 +92,6 @@ def cours_dollar():
         return 'dollard augmente'
     else:
        return 'dollard baisse  '
-
-
-
-
-
-#print(recup_balise())
-
-
 
 
 
