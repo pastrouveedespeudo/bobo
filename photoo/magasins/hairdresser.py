6ca32b30c2282f395ad6f1bb4eba32015f84062d
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-16 19:22:16 +0200

    Update and rename coiffeur.py to hairdresser.py

--- photoo/magasins/hairdresser.py
+++ photoo/magasins/hairdresser.py
@@ -9,68 +9,20 @@ LISTE1 = ['coiffure', 'COIFFURE', 'salon', 'salon de coiffure',
           'Coiffeur', 'Hairdressers', 'coiffeuse']
 
 
-
-
-
-def rayon(ville):
-    path = "https://www.villorama.com/ville/{}/villes-proches.html"
-    
-    path = path.format(ville)
-    r = requests.get(path)
-    page = r.content
-    
-    soup = BeautifulSoup(page, "html.parser")
-    
-    propriete = soup.find_all("a")
-
-    liste = []
-
-    for i in propriete:
-        if i.string == None:
-            pass
-        elif i.string == 'France' or i.string == 'Distance à vol d\'oiseau'\
-             or i.string == 'Communes proches' or i.string == 'Présentation'\
-             or i.string == 'En bref' or i.string == 'Sport'\
-             or i.string == 'Météo' or i.string == 'Prévisions météo'\
-             or i.string == 'Sorties culturelles' or i.string == 'Agenda'\
-             or i.string == 'Hôtels' or i.string == 'Midi Moins Une'\
-             or i.string == 'mentions légales' or i.string == 'Recrutement'\
-             or i.string == 'Midi Moins Une' or i.string == 'Gestion des cookies'\
-             or i.string == 'Tourisme' or i.string == 'Office de Tourisme'\
-             or i.string == 'Distance routière' or i.string == 'Mairie'\
-             or i.string == 'Plan' or i.string == 'Itinéraire' or i.string == 'Annuaire Services'\
-             or i.string == 'Résultats d\'élections' or i.string == 'Loisirs' or i.string == 'Spectacles'\
-             or i.string == 'Concerts' or i.string == 'Théâtre' or i.string == 'Historique météo'\
-             or i.string == 'Rencontres' or i.string == 'Autres informations' or i.string == 'Cinéma'\
-             or i.string == 'Sites web' or i.string == 'Identifiez-vous' or i.string == 'Votre espace'\
-             or i.string == 'Publicité' or i.string == '' or i.string == 'Conditions d\'Utilisation'\
-             or i.string == 'Etablir un lien':
-             
-             
-            pass
-             
-        else:
-            liste.append(i.string)
-
-    liste = liste[3:]
-    
-    return liste #villes aux alentours
-
-
-def ville(ville):
+def cities(city):
     path = 'https://www.google.com/search?q={}+coiffeur&oq={}+coiffeur'
-    path = path.format(ville, ville)
+    path = path.format(city, city)
 
     r = requests.get(path)
     page = r.content
     
     soup = BeautifulSoup(page, "html.parser")
     
-    propriete = soup.find_all("span")
+    Property = soup.find_all("span")
 
     liste = []
 
-    for i in propriete:
+    for i in Property:
         print(i.string)
         for j in LISTE1:
             a = str(i.string).find(str(j))
@@ -79,16 +31,14 @@ def ville(ville):
     print(liste)
     return liste #coiffeur dans ces villes
 
-def horraire(nom, ville):
+def schedule_hair(name, city):
 
     path = 'https://www.google.com/search?ei=Gnn5XJP5KIu5gweW1qaQBQ&q={0}+{1}+horraires&oq={0}+{1}+horraires'
 
-    path = path.format(nom, ville)
+    path = path.format(name, city)
 
     #print(path)
 
-
-    
     r = requests.get(path, headers={
          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"
      })
@@ -96,14 +46,15 @@ def horraire(nom, ville):
 
     soup = BeautifulSoup(page, "html.parser", from_encoding="utf-8")
 
-    propriete = soup.find_all("td")
+    Property = soup.find_all("td")
 
     liste = []
-    for i in propriete:
+    for i in Property:
+        print(i.string)
         liste.append(i.string)
 
-##
-##    semaine = ['lundi',
+
+##    week = ['lundi',
 ##              'mardi',
 ##              'mercredi',
 ##              'jeudi',
@@ -114,24 +65,24 @@ def horraire(nom, ville):
 
 
 
-    semaine = ['Monday',
-              'Tuesday',
-              'Wednesday',
-              'Thursday',
-              'Friday',
-              'Saturday',
-              'Sunday',
+    week = ['Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday',
              ]
 
-    jour = ''
-
+    day = ''
+    p = ''
     liste1 = []
     c = 0
     for i in liste:
-        heure = ''
+        hour = ''
         #print(i)
         #print('\n')
-        for j in semaine:
+        for j in week:
             a = str(i).find(str(j))
             if a >= 0:
                 if i == 'Monday':
@@ -155,9 +106,12 @@ def horraire(nom, ville):
         c+=1
       
        
-
+    print(liste1)
     return liste1
 
+
+
+#HERE THERE ARE OPTIONAL function we dont call it
 def numero(nom, ville):
     
     path = "https://annuaire.118712.fr/?s={}+{}"
@@ -218,6 +172,50 @@ def numero(nom, ville):
 
 
 
+def rayon(ville):
+    path = "https://www.villorama.com/ville/{}/villes-proches.html"
+    
+    path = path.format(ville)
+    r = requests.get(path)
+    page = r.content
+    
+    soup = BeautifulSoup(page, "html.parser")
+    
+    propriete = soup.find_all("a")
+
+    liste = []
+
+    for i in propriete:
+        if i.string == None:
+            pass
+        elif i.string == 'France' or i.string == 'Distance à vol d\'oiseau'\
+             or i.string == 'Communes proches' or i.string == 'Présentation'\
+             or i.string == 'En bref' or i.string == 'Sport'\
+             or i.string == 'Météo' or i.string == 'Prévisions météo'\
+             or i.string == 'Sorties culturelles' or i.string == 'Agenda'\
+             or i.string == 'Hôtels' or i.string == 'Midi Moins Une'\
+             or i.string == 'mentions légales' or i.string == 'Recrutement'\
+             or i.string == 'Midi Moins Une' or i.string == 'Gestion des cookies'\
+             or i.string == 'Tourisme' or i.string == 'Office de Tourisme'\
+             or i.string == 'Distance routière' or i.string == 'Mairie'\
+             or i.string == 'Plan' or i.string == 'Itinéraire' or i.string == 'Annuaire Services'\
+             or i.string == 'Résultats d\'élections' or i.string == 'Loisirs' or i.string == 'Spectacles'\
+             or i.string == 'Concerts' or i.string == 'Théâtre' or i.string == 'Historique météo'\
+             or i.string == 'Rencontres' or i.string == 'Autres informations' or i.string == 'Cinéma'\
+             or i.string == 'Sites web' or i.string == 'Identifiez-vous' or i.string == 'Votre espace'\
+             or i.string == 'Publicité' or i.string == '' or i.string == 'Conditions d\'Utilisation'\
+             or i.string == 'Etablir un lien':
+             
+             
+            pass
+             
+        else:
+            liste.append(i.string)
+
+    liste = liste[3:]
+    
+    return liste #villes aux alentours
+
 
 
 
@@ -233,7 +231,7 @@ def numero(nom, ville):
 #print(alentour)
 ##
 
-#les_coiffeurs = ville('vénissieux')
+#les_coiffeurs = ville('crest')
     
     
 
