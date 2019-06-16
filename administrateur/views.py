805d46c4c2f1fb33e731a178864298f6463d94bf
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-16 19:40:49 +0200

    Update views.py
    oh c trop chiant putin

--- administrateur/views.py
+++ administrateur/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
 import os
 import cv2
-
 import json
+
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files import File
+from django.http import JsonResponse
+
+from .models import *
+from .forms import imagepost
+
+
+from static.bobo.coupe_analysis import *
+from static.bobo.mode_analyse import *
+
+from .tendance_site.mode_w_data import *
 
 
 try:
@@ -11,35 +22,8 @@ try:
 except:
     pass
 
-from django.core.files import File
-
-from .models import *
-from .forms import imagepost
-
-from django.http import JsonResponse
-
-from static.bobo.mode_w_data import haut_bas
-from static.bobo.coupe_analysis import *
-from static.bobo.mode_analyse import *
-#from static.bobo.analyse_femme_haut import *
-from static.bobo.database import supprimer_database
-
-
-def mode(request):
-       
-    return render(request, "mode.html")
-
 
 def database_mode(request):
-
-    if request.method == "POST":
-        print('ouiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
-        supprimer = request.POST.get('suppression')
-        print(supprimer)
-        if supprimer == 'supp':
-            supprimer_database()
-        
-        return HttpResponse('ok')
 
     data = recup()#from mode_analyse.py
     data_coupe = recup2()#from coupe_analysis
@@ -54,8 +38,11 @@ def database_mode(request):
     liste = os.listdir()
 
     liste1 = []
-    laliste1 = []
+    liste11 = []
+    theliste1 = []
 
+    #We ignore this files
+    #or add it to laliste
     for i in liste:
         if i == 'analyse_femme_haut.py' or  i == 'bobo.txt' or\
            i == 'config.py' or i == 'constante.py' or i == 'conteneur.py' or\
@@ -66,31 +53,28 @@ def database_mode(request):
            or i =='analysa.py' or i == 'tendance.py':
             pass
         else:
-            laliste1.append(i)
+            theliste1.append(i)
 
-    laliste1 = sorted(laliste1)
+    theliste1 = sorted(theliste1)
 
+    #We add picture,
     c = 0
-    for i in laliste1:
+    for i in theliste1:
         try:
             liste1.append((str(laliste1[c]), str(laliste1[c+1]), int(str(c) + str(c))))
             c+=2
         except:
             pass
         
-
     try:
         os.chdir('/app/static/bobo')
     except:
         os.chdir(r'C:\Users\jeanbaptiste\bobo\bobo\static\bobo')
 
-        
     listee = os.listdir()
     
-    liste11 = []
-    
     c = 0
-    for i in laliste1:
+    for i in theliste1:
 
         try:
             liste11.append((str(laliste1[c]), str(laliste1[c+1]), int(str(c) + str(c))))
@@ -99,11 +83,8 @@ def database_mode(request):
             pass
 
 
-    
     a = haut_bas()
- 
     b = recup2()
- 
     c = liste11
 
     liste_finale = []
@@ -135,7 +116,11 @@ def database_mode(request):
 
 def tendance(request):
     
-    os.chdir('/app/static/bobo')
+    try:
+        os.chdir('/app/static/bobo')
+    except:
+        os.chdir(r'C:\Users\jeanbaptiste\bobo\bobo\static\bobo')
+        
     liste = os.listdir()
 
     liste1 = []
@@ -197,78 +182,6 @@ def tendance(request):
 
 
 
-
-
-def ajout(request):
-
-
-    form = imagepost(request.POST or None, request.FILES or None)
-    context = {'form':form}
-
-    try:
-        if request.method == "POST":
-            mode = request.POST.get('hidden')
-
-            liste = os.listdir('/app/static/bobo')
-            
-            liste_max = []
-
-            
-            for i in liste:
-                print(i)
-            
-                try:
-                    print(str(i[0]) + str(i[1]))
-                    a = str(i[0]) + str(i[1])
-                    a = int(a)
-                
-                    liste_max.append(a)
-                    
-                except:
-                    a = str(i[0])
-                    a = int(a)
-                    liste_max.append(a)
-            
-         
-
-            num = max(liste_max)
-
-            new_number = int(num) + 1
-        
-
-            if mode == 'vetement':
-                nouveau = str(new_number) + 'a.jpg'
-                
-            elif mode == 'coupe':
-                num = max(liste_max)
-                new_number = int(num)
-                nouveau = str(new_number) + 'b.jpg'
-
-            if form.is_valid():
-                im = form.save(commit=False)
-                im.save()
-        
-                liste = os.listdir('/app/static/bobo')
-                os.chdir('/app/static/bobo')
-                img = cv2.imread(liste[-1])
-                cv2.imwrite(nouveau, img)
-
-                os.remove(liste[-1])
-                print('renamed')
-                
-            return render(request, "mode.html", context)
-    except:
-        pass
-
-    
-    return render(request, "ajout.html", context)
-
-
-
-
-
-def essais(request):
-    return render(request, "essais.html")
 
 
 
