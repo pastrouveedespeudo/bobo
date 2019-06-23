88b82980607005f54b6b4669e3b79342de5bca34
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-23 16:24:39 +0200

    Update urls.py
    allez plus que le vent

--- polu/urls.py
+++ polu/urls.py
@@ -14,6 +14,13 @@ urlpatterns = [
     path(r'/machine_a_o', views.machine_a_o),
     path(r'/prediction', views.prediction),
     path(r'/info_pollu', views.info_pollu),
+    path(r'/navebarre_donnee', views.navebarre_donnee),
+    path(r'/navebarre_graphe', views.navebarre_graphe),
+    path(r'/navebarre_info', views.navebarre_info),
+    path(r'/navebarre_prediction', views.navebarre_prediction),
+    path(r'/navebarre_soluce', views.navebarre_soluce),
+    path(r'/navebarre_vent', views.navebarre_vent),
+    
 
     
 ]
