3a150aad6f33feb3d6d2a81107f30dd422fdcb1e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-25 21:05:40 +0200

    Update address.py

--- photoo/magasins/address.py
+++ photoo/magasins/address.py
@@ -1,22 +1,32 @@
+"""Here we searching address for Gym
+or Hairdressers. So we can
+display it on a google map"""
+
 import requests
 from bs4 import *
 from geopy.geocoders import Nominatim
 
-scrap = ['Rue', 'Avenue', 'Allée', 'allée', 'Impasse', 'Adresse']
+#We define global variables
+SCRAP = ['Rue', 'Avenue', 'Allée', 'allée', 'Impasse', 'Adresse']
+AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"
+PATH = "https://www.google.com/search?q=adresse+{}+{}+&oq=adresse+{}+{}"
+
+
 
 def address_geo(name, city):
-    path = "https://www.google.com/search?q=adresse+{}+{}+&oq=adresse+{}+{}"
-    path = path.format(name, city, name, city)
+    """We scrapping by SCRAP variable
+    if we find one of those
+    we break and recup the line"""
+    
+    path = PATH.format(name, city, name, city)
 
-    r = requests.get(path, headers={
-         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"
-     })
-    page = r
-    page = r.content
+    request_html = requests.get(path, headers={"User-Agent": AGENT})
+
+    page = request_html.content
     
-    soup = BeautifulSoup(page, "html.parser")
+    soup_html = BeautifulSoup(page, "html.parser")
     
-    propriete = soup.find_all("span")
+    propriete = soup_html.find_all("span")
 
     stop = ''
     address = []
@@ -27,13 +37,12 @@ def address_geo(name, city):
         if i.string == None:
             pass
         else:
-            for j in scrap:
-                a = str(i.string).find(str(j))
-                if a >= 0:
+            for j in SCRAP:
+                finding = str(i.string).find(str(j))
+                if finding >= 0:
                     address.append(i.string)
                     stop = True
                     
-    print(address)
     return address
 
 
@@ -41,29 +50,21 @@ def city_geo(parameter):
     """Here we searching from Python modul(geopy.geocoders)"""
     """address from the input from html page"""
 
-    print(parameter)
     geocoder = Nominatim(user_agent="app.py")
-    #parametre is data recup from data()
     
     location = geocoder.geocode(parameter, True, 30)
     localisation = location.address
     localisation = str(localisation)
 
-    #define data from geopy.geocoders into var
-
     b = location.latitude
     c = location.longitude
 
     
-    print(b,c)
     return b, c
 
 
 
 
-
-#address_geo('Hair salon · Avenue Félix Rozier', 'crest')
-city_geo('Avenue Félix Rozier, 26400 Crest')
 
 
 
