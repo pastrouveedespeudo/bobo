5b98e8270d4b6d56080c9209b7b212ae7d3706f2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-25 20:47:14 +0200

    Add files via upload

---
+++ photoo/magasins/gym_config.py
@@ -0,0 +1,7 @@
+GYM = ['fitness', 'gym', 'body', 'Body', 'musculation', 'Fitness', 'Club', 'Gym',
+       'sport', 'Sport', 'Salle', 'salle', 'Salle de sport', 'salle de sport', 'wellness',
+       'Wellness', 'Keep cool', 'KEEP COOL', 'keep cool', ]
+
+PATH_CITY = "https://www.google.com/search?ei=w2n6XJ3hMMeOatXLjOgB&q=sale+de+gym+{0}+france&oq=sale+de+gym+{0}+france"
+PATH_SCHEDULE = 'https://www.google.com/search?ei=Gnn5XJP5KIu5gweW1qaQBQ&q={0}+{1}+horraires&oq={0}+{1}+horraires'
+AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"
