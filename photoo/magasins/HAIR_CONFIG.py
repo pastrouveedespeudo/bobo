3813637e682f28928ab303aa2ad593a937b5d1f2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-25 20:59:34 +0200

    Add files via upload
    merde les com's

---
+++ photoo/magasins/HAIR_CONFIG.py
@@ -0,0 +1,10 @@
+LISTE1 = ['coiffure', 'COIFFURE', 'salon', 'salon de coiffure',
+         'Franck Provost', 'coiffeur', 'hairdresser', 'prestige', 'Prestige',
+          'hair', 'bambo', 'haircut', 'Hair', 'Hairdresser', 'Coiffure',
+          'Coiffeur', 'Hairdressers', 'coiffeuse']
+
+PATH_HAIRDRESSER = 'https://www.google.com/search?q={}+coiffeur&oq={}+coiffeur'
+
+PATH_SCHEDULE = 'https://www.google.com/search?ei=Gnn5XJP5KIu5gweW1qaQBQ&q={0}+{1}+horraires&oq={0}+{1}+horraires'
+
+AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"
