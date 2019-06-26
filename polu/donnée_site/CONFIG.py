e67a3c0bbd4ac21009e2ced8f982b074a01e7822
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-26 22:31:56 +0200

    Add files via upload
    finir demain faire de la page mtn

---
+++ polu/donnée_site/CONFIG.py
@@ -0,0 +1,76 @@
+CLE = '5a72ceae1feda40543d5844b2e04a205'
+PATH_PARTICLE_RATE = "https://air.plumelabs.com/fr/live/{}"
+
+
+HEURE_POINT_WEEK = [7,8,9,16,17,18,19]
+
+DEAPARTURE = [(2,1), (5,1), (9,2), (16,2), (22,2),(23,2),
+                  (1,3),(2,3),(8,3),(9,3),
+                  (19,4),(22,4),(26,4),(27,4),(28,4),
+                  (4,5),(5,5),(29,5),(30,5),
+                  (5,6),(6,6),(7,6),(10,6),(28,6),
+                  (5,7),(6,7),(7,7),(12,7),(13,7),(14,7),(19,7),(20,7),(21,7),(26,7),(27,7),(28,7),
+                  (2,8),(3,8),(4,8),(9,8),(10,8),(11,8),(16,8),(17,8),(18,8),(19,8),(23,8),(24,8),(25,8),(30,8),(31,8),
+                  (1,9),
+                  (18,10),(25,10),(26,10),(31,10),
+                  (3,11),(8,11),(11,11),
+                  (20,12),(21,12),(22,12),(24,12),(27,12),(28,12)
+             ]
+
+PRESSURE_PATH = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}"
+WEATHER_PATH = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}"
+CLIMATE_PATH = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}"
+
+
+POLLUTING_POLE = ['Nord',
+                'Bouches-du-Rhône',
+                'Moselle',
+                'Seine-Maritime',
+                'Loire-Atlantique',
+                'Haute-Normandie',
+                'Meurthe-et-Moselle',
+                'Seine-Maritime',
+                'Rhône'
+                ]
+
+
+PATH_WIKI = "https://fr.wikipedia.org/wiki/{}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
