0c45136c6b7cb755d1865f038a855d7e980cff93
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-26 12:40:29 +0200

    Update views_functions.py

--- photoo/views_functions.py
+++ photoo/views_functions.py
@@ -9,16 +9,9 @@
 #is content into static.bobo becase
 #they are functions
 #who trait picture
+
 try:
-    from static.bobo.tendance import dataaa
-    from static.bobo.tendance import i_into_i
-    from static.bobo.tendance import unification
-    from static.bobo.tendance import suppression_en_trop
-    from static.bobo.tendance import re_elment_de_liste
-    from static.bobo.tendance import mise_en_dico
-    from static.bobo.tendance import determination_couleur
-    from static.bobo.tendance import les_tendances_couleurs
-    from static.bobo.tendance import analyse_tendance  
+    from static.bobo.tendance import la_tendance
 except:
     pass
 
@@ -31,16 +24,8 @@ from .magasins.hairdresser import *
 
 def the_colors_function(color):
 
-    liste = dataaa()
-    liste1 = i_into_i(liste)
-    liste2 = unification(liste1)
-    liste3 = suppression_en_trop(liste2)
-    liste6 = re_elment_de_liste(liste3)
-    liste7 = mise_en_dico(liste6)
-    liste8 = determination_couleur(liste7)
-    liste9 = les_tendances_couleurs(liste8)
-    liste10 = analyse_tendance(liste9)
-    
+    liste10 = la_tendance()
+
     if color == 'blonde':
         coul_analyse_haut = liste10[1][0]
         coul_analyse_bas = liste10[1][1]
@@ -128,7 +113,6 @@ def map_hairdresser_function(map_hairdresser, vivile):
         data = str(lat_long[0]) + ' ' + str(lat_long[1])
     
     return data
-
 
 
 
