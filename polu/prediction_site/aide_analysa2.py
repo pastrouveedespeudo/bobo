6d2b5caeb7e4bfd077fa2a4040fe100ff1bc075a
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-27 11:50:10 +0200

    Update aide_analysa2.py
    j'ai révé que jretourné a lyon et que jconnaissai pas le code oh puté ca m'a mis un coup de foué

--- polu/prediction_site/aide_analysa2.py
+++ polu/prediction_site/aide_analysa2.py
@@ -1,27 +1,35 @@
 import psycopg2
 from statistics import *
 
+from .CONFIG import DATABASE
+from .CONFIG import USER
+from .CONFIG import HOST
+from .CONFIG import PASSWORD
+
 
 def vision(ville):
     
-    conn = psycopg2.connect(database='datu8fkornnndh',
-                            user='pwtfmpvfpsujtw',
-                            host='ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
-                            password='e260133d94ee203ca0d3d7f0ccbc37d20b27b63b06841ca37a4e42eaf9ef5696')
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD)
 
     
     cursor = conn.cursor()
 
 
-
-
-    
-    cursor.execute("""select angrais, saison, diesel, eruption, jour_nuit,
-                   TRAFIQUE, HEURE, WEEKEND, BOUCHON, ACTIVITE_EXEPTIONNELLE,
-                   POPULATION_ACTIVE_HABITANT, REGION_INDUSTRIEL_POLLUEE,
-                   polenne, pos, météo, vent, pression, climat, incendie,
-                   nombre_particule
-                   from conditions2 where nom_ville=%s""", (ville,))
+    cursor.execute("""select angrais,
+                    saison, diesel,
+                    eruption, jour_nuit,
+                    TRAFIQUE, HEURE,
+                    WEEKEND, BOUCHON,
+                    ACTIVITE_EXEPTIONNELLE,
+                    POPULATION_ACTIVE_HABITANT,
+                    REGION_INDUSTRIEL_POLLUEE,
+                    polenne, pos, météo,
+                    vent, pression, climat,
+                    incendie, nombre_particule
+                    from conditions2 where nom_ville=%s""", (ville,))
 
 
     conn.commit()
