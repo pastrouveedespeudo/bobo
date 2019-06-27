b8756819310cb66eb34be6f3ef8d5ffeea5306f5
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-27 10:30:19 +0200

    Update polenne.py

--- polu/donnée_site/polenne.py
+++ polu/donnée_site/polenne.py
@@ -1,77 +1,91 @@
 """BS4, requests, datatime 16/04/2019"""
 
-import os
-import cv2
-import json
+
+
+
 import requests
-import datetime
+
 import urllib.request
 from bs4 import *
-from colour import Color
 
 
-#Path to site of polenne
-PATH_PARIS = 'https://weather.com/fr-FR/forecast/allergy/l/1975528fb7e3553b7eacfe7ac89b421986bb9949c2506b144b4e228d57da124b'
-PATH_LYON = 'https://weather.com/fr-FR/forecast/allergy/l/7615c204059d6d10382d733bf8dc1718bcac1e82e2f2cf66e6842581ca9360c2'
-PATH_MARSEILLE = 'https://weather.com/fr-FR/forecast/allergy/l/97adc36f89aa35486ece34380b006f2c946ef82fad53a58954c33e39e23948fe'
+
+from CONFIG import PATH_PARIS_POLENNE
+from CONFIG import PATH_LYON_POLENNE
+from CONFIG import PATH_MARSEILLE_POLENNE
+from CONFIG import COMPA_POELENNE
 
 
-def polenne(city):
-
+def path_function(city):
+    """Here we define the path"""
+    
     city = city.lower()
 
-    #according to the city we grant the path
+    path = ''
+    
     if city == 'lyon':
-        path = PATH_LYON
+        path = PATH_LYON_POLENNE
 
     elif city == 'paris':
-        path = PATH_PARIS
+        path = PATH_PARIS_POLENNE
         
     elif city == 'marseille':
-        path = PATH_MARSEILLE
-    
-    #BS4 stuff, we recup all div from the
-    #HTML page.
-    r = requests.get(path)
-    page = r.content
-    soup = BeautifulSoup(page, "html.parser")
-    Property = soup.find_all("div")
+        path = PATH_MARSEILLE_POLENNE
 
-    #We searching "Confort de respiration"
-    #And take the finding to finding + 200
+    return path 
+
+
+def soup_function(city):
+    """Soup and BS4 call"""
     
+    #path_function()
+    path = path_function(city)
+
+    request_html = requests.get(path)
+    page = request_html.content
+    soup_html = BeautifulSoup(page, "html.parser")
+    Property = soup_html.find_all("div")
+
+
     liste = []
     liste.append(str(Property))
-    a = str(liste).find('Confort de respiration:')
-    liste = liste[0][a:a+200]
+    
+    finding = str(liste).find('Confort de respiration:')
+    liste = liste[0][finding:finding + 200]
+
+    return liste
+
+def polenne(city):
+    """we are looking for the rate of polenne"""
+
+    #soup_function()
+    liste = soup_function(city)
 
     word = ''
-    compa = '<div class="styles__allergyMsg__qual__yaxvL" classname="styles__allergyMsg__qual__yaxvL"'
-
     liste2 = []
-
-    #Our searched word is just after the compa variable into liste
-    #so, we increment mot,
-    #if i == > or < (tag) we verify if mot == compa
-    #if yes we defining a to True.
-    #If a == True, we add to liste2 the world searched.
+    oContinuer = ''
     
     for i in liste:
         if i == '>':
-            if word == compa:
+            if word == COMPA_POELENNE:
                 word = ''
-                a = True
+                oContinuer = True
             else:
                 word =  ''
 
-        elif a == True:
+        elif oContinuer == True:
             if i == '<':
-                a = False
+                oContinuer = False
             liste2.append(i)
         else:
             word += i
 
     return "".join(liste2[:-1])
+
+
+
+
+
 
 
 
