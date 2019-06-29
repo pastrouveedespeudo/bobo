9aaacb0f057cc78c4e352f39bbd5f0547e780416
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-29 19:33:03 +0200

    Add files via upload
    ok texte ce soir et js en gros faut refactoriser et mettre dans un dossier a part ! !
    
    le bug du boutton le faire en meme temps

---
+++ direction/carte/direction/CONFIG.py
@@ -0,0 +1,8 @@
+CLE = '5a72ceae1feda40543d5844b2e04a205'
+PATH_VENT = "http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}"
+PATH_POSTAL = 'http://code.postal.fr/code-postal-{}'
+PATH_VENT_DEUX = 'https://www.lameteoagricole.net/meteo-heure-par-heure/{}-{}.html'
+VENT_DEUX_RECHERCHE = '<td class="td_corps_meteo" width="63"><img alt="direction du vent - '
+
+
+PATH_SUPERFICIE = 'https://www.google.com/search?ei=wjnsXOzeIZCAjLsPm62CgAo&q={}+superficie&oq={}+superficie'
