bd3a1e54076a51bae015e49dbd91ef2b7b1cfc43
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-29 17:15:46 +0200

    Update main.py

--- direction/carte/main.py
+++ direction/carte/main.py
@@ -1,4 +1,3 @@
-
 from direction.ville import ville
 from direction.nouvel_pos import long_lat
 from direction.boussole import calcul_vent
@@ -9,83 +8,43 @@ from direction.addresse import *
 
 
 if __name__=="__main__": 
-    liste = ['Jardin']
+  
 
 
-    new_lat = ''
-    new_long = ''
+    def runner(liste):
 
+        new_lat = ''
+        new_long = ''
 
-    while True:
+        while True:
 
-        for i in liste:
-            
-            if new_lat != '' and new_long != '':
+            for i in liste:
                 
-                print('de lattitude:', new_lat, 'et de longitude', new_long)
+                if new_lat != '' and new_long != '':
+
+                    adresse = dress_to_ville(new_lat, new_long)
+                    position_vent = vent_deux(adresse[0])
+                    vent = superficie_ville(adresse[1])
+                    print(vent)
+
+                else:
+             
+                    lat, long = ville(i)
+                    adresse = dress_to_ville(lat, long)
+                    position_vent = vent_deux(i)
+                    vent = superficie_ville(adresse[1])
+                    print(vent)
+
+
+                if new_lat != '' and new_long != '':
+                    
+                    nouvelle_position = long_lat(new_lat, new_long, vent, position_vent)
+                    
+                else:
+                          
+                    nouvelle_position = long_lat(lat, long, vent, position_vent)  
                 
-                adresse = dress_to_ville(new_lat, new_long)
-      
-                print('adresse trouvée :', adresse[0])
-
-                position_vent = vent_deux(adresse[0])
-                
-                vent = superficie_ville(adresse[1])
-                
-                
-                
-            else:
-                
-                lat, long = ville(i)
-       
-                adresse = dress_to_ville(lat, long)
-          
-                print('de lattitude:', lat, 'de longitude', long)
-                
-                position_vent = vent_deux(i)
-                
-                vent = superficie_ville(adresse[1])
+                new_lat = nouvelle_position[0]
+                new_long = nouvelle_position[1]
 
                 
-               
-            print(vent)
-            print('donc le vent va vers:', position_vent)
-           
-            
-           
-            
-            if new_lat != '' and new_long != '':
-                
-                nouvelle_position = long_lat(new_lat, new_long, vent, position_vent)
-                print('la nouvelle position est de :', nouvelle_position)
-                
-            else:
-                      
-                nouvelle_position = long_lat(lat, long, vent, position_vent)
-                print('la nouvelle position est de :', nouvelle_position)    
-            
-            new_lat = nouvelle_position[0]
-            new_long = nouvelle_position[1]
-
-            
-            print('\n')
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
